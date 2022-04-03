312d1dcea351f238c7c02c8472c6996afa7968cf
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-02 21:27:14 -0400

    Finished Day 1 :)

---
+++ Day 1.py
@@ -0,0 +1,21 @@
+"""
+Day 1: Band Name Generator
+"""
+
+# I guess I really am going back to the basics
+
+print('Welcome to the Band Name Generator!')
+hometown = input('What was the town you grew up in?\n')
+pet_name = input('What the name of your first pet?\n')
+print('Your band name is', hometown, pet_name,'\nRock on!')
+
+
+"""
+Welcome to the Band Name Generator!
+What was the town you grew up in?
+Bowling Green
+What the name of your first pet?
+Soup
+Your band name is Bowling Green Soup 
+Rock on!
+"""
