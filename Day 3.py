bc19e148392a677a09a19aa1057605f632d4ca99
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-05 19:02:19 -0400

    Improved print timing and input displays

--- Day 3.py
+++ Day 3.py
@@ -30,58 +30,75 @@ print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
 # Begin my code
+print_delay = 0.25
 
-ready = input('Are you ready?! (Y/N)\n').lower()
+
+ready = input('Are you ready?! (Y/N) ---> ').lower()
 if ready == 'yes' or ready == 'y':
     print('\"Arrrrrr, welcome aboard matey! You will be sharing a bunk with one-eyed Jane.\"')
 else:
-    print('\"Then we shall have to do some kidnapping. Into the barrel ye goes!\"')
+    print('\"Then we shall have to do snome kidnapping. Into the barrel ye goes!\"')
+    time.sleep(print_delay)
     print('You are grabbed and thrown into an empty barrel labelled \"salted pork\".')
-
-time.sleep(5)
-
+time.sleep(print_delay)
 print('You accept your fate and try to get some sleep despite the smell.')
+time.sleep(print_delay)
 print('After some bad rest, you are woken up by a strong lurch that knocks you on the floor.')
+time.sleep(print_delay)
 print('You scuttle away and find that the ship had struck land. \"Is that how ships work?\" you think to yourself.')
+time.sleep(print_delay)
 print('From over your shoulder you hear the captain say, \"Welcome to Treasure Island!\" '
       'Everyone on the boat begins jumping overboard.')
+time.sleep(print_delay)
 
-jump = input('Do you jump off the right or left side of the boat? (R/L)\n').lower()
+jump = input('Do you jump off the right or left side of the boat? (R/L) ---> ').lower()
 if jump == 'l' or jump == 'left':
     print('Good choice! You safely swim ashore with your new pirate friends.')
+    time.sleep(print_delay)
 
     pond = input('Walk up the beach and into the jungle. There you come across a clear pond so clear you can see the'
-                 'bottom. Do you wash off the sand on you? (Y/N)\n').lower()
+                 ' bottom. Do you wash off the sand on you? (Y/N) ---> ').lower()
     if pond == 'no' or pond == 'n':
         ('Gross.')
+        time.sleep(print_delay)
 
         print('After some more walking, a clearing appears. In the middle is what looks like an abandoned house.')
+        time.sleep(print_delay)
         print('Your party approaches and discovers three doors: red, yellow, and blue.')
+        time.sleep(print_delay)
         print('Someone yells, \"Let the new kid choose!\" You freeze and everyone turns to you.')
+        time.sleep(print_delay)
         print('\"Go on\", says the captain drawing his gun. You take a deep breath and step forward.')
+        time.sleep(print_delay)
 
-        door = input('Which door do you choose? (R/Y/B)\n').lower()
+        door = input('Which door do you choose? (R/Y/B) ---> ').lower()
         if door == 'blue' or door == 'b':
             print('A troupe of monkeys clamor out and devour you. Whoopsie.')
+            time.sleep(print_delay)
             print('Game Over.')
         elif door == 'red' or door == 'r':
             print('Tongues of fire leap out and consume you. This is not your day.')
+            time.sleep(print_delay)
             print('Game Over.')
         elif door == 'yellow' or door == 'y':
             print('Tada, you found the gold! You will be rewarded for your forced bravery.')
+            time.sleep(print_delay)
             print('You win!')
         else:
             print('That was not an option. The captain shoots you for not following directions. Pirates are very '
                   'particular about following directions.')
+            time.sleep(print_delay)
             print('Game Over.')
 
     else:
         print('Immediately, trout come out from all the rocks and attack you. "So that\'s why it is called \'Long John'
               'Silvers\'!", you yell before being overwhelmed and drown.')
+        time.sleep(print_delay)
         print('Game Over.')
 
 else:
     print('You didn\'t look first and jumped right onto land, breaking both femurs. Ouch!')
+    time.sleep(print_delay)
     print('Game Over.')
 
 
