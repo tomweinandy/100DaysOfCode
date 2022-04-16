7ad1a288efccef659a320ee84a42beb24599179d
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-16 11:45:07 -0400

    Fixed html encoding issue

--- Day17/main.py
+++ Day17/main.py
@@ -1,16 +1,36 @@
+"""
+Day 17: Geography Quiz
+"""
 from question_model import Question
-from data import question_data
+from data import *
 from quiz_brain import *
+import random
 
-question_list = []
-for q in question_data:
-    question_list.append(Question(q['text'], q['answer']))
+# Continue for multiple games, if desired
+proceed = True
+while proceed:
 
+    # Generate list of initialized questions
+    question_list = []
+    for q in question_data_geography['results']:
+        question_list.append(Question(q['question'], q['correct_answer']))
+    random.shuffle(question_list)
 
-quiz = QuizBrain(question_list)
-correct = True
+    # Initialize Quiz
+    quiz = QuizBrain(question_list)
+    correct = True
 
-while correct and not quiz.check_end():
+    # Begin quiz
+    while correct and not quiz.check_end():
+        guess = quiz.ask_question()
+        correct = quiz.check_guess(guess)
 
-    guess = quiz.ask_question()
-    correct = quiz.check_guess(guess)
+    # Ask if player wants to play again
+    play_again = input('\nPlay again? (True/False): ')
+    print('\n')
+    play_again = convert_t_or_f(play_again)
+
+    if play_again == 'false':
+        proceed = False
+
+print('The end.')
