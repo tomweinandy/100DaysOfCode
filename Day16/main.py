03da8b7614d11ec99c13a32ce2e6d2c5aaf13a27
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-16 08:36:18 -0400

    Check resources sufficient

--- Day16/main.py
+++ Day16/main.py
@@ -15,15 +15,82 @@ from money_machine import MoneyMachine
 #     coffee=24
 # )
 
+# Initialize class objects
 menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 
-# Separate drinks into list (removing last item, which is empty)
-menu_list = menu.get_items().split('/')[:-1]
 
-# Force correct drink order from list
-order = ''
-while order not in menu_list:
+# Set initial conditions
+# order = ''
+menu_list = menu.get_items().split('/')[:-1]  # Separate drinks into list (removing last item, which is empty)
+input_list = menu_list + ['report', 'off']  # Add additional functions to input
+# # drink_list = [k for k in menu.keys()]
+# ingredient_list = [k for k in resources.keys()]
+# resources['money'] = 0
+# menu['espresso']['ingredients']['milk'] = 0
+powered_on = True
+
+
+# Machine operates while powered on
+while powered_on:
+
     order = input(f'What would you like? ({menu.get_items()}): ')
 
+    # Ask again if order is invalid
+    while order not in input_list:
+        order = input(f'What would you like? ({menu.get_items()}): ')
 
-print(menu_list)
+    if order == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    #
+    elif order in menu_list:
+        drink = menu.find_drink(order)
+        print(coffee_maker.is_resource_sufficient(drink))
+
+        if coffee_maker.is_resource_sufficient(drink):
+            coffee_maker.make_coffe(drink)
+            print()
+        else:
+            print('Sorry, insufficient ingredients.')
+
+
+
+
+
+
+    elif order == 'off':
+        powered_on = False
+
+
+
+# # todo 4. Check resources sufficient?
+#     a. When the user chooses a drink, the program should check if there are enough resources to make that drink.
+#     b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should not continue to make the drink but print: “Sorry there is not enough water.”
+#     c. The same should happen if another resource is depleted, e.g. milk or coffee.
+# # todo 5. Process coins.
+#     a. If there are sufficient resources to make the drink selected, then the program should prompt the user to insert coins.
+#     b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
+#     c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
+#         pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
+# # todo 6. Check transaction successful?
+#     a. Check that the user has inserted enough money to purchase the drink they selected. E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the program should say “Sorry that's not enough money. Money refunded.”.
+#     b. But if the user has inserted enough money, then the cost of the drink gets added to the machine as the profit and this will be reflected the next time “report” is triggered. E.g. Water: 100ml
+#         Milk: 50ml
+#         Coffee: 76g
+#         Money: $2.5
+#     c. If the user has inserted too much money, the machine should offer change.
+#         E.g. “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal places.
+# # todo 7. Make Coffee.
+#     a. If the transaction is successful and there are enough resources to make the drink the user selected, then the ingredients to make the drink should be deducted from the coffee machine resources.
+#         E.g. report before purchasing latte: Water: 300ml
+#             Milk: 200ml
+#             Coffee: 100g
+#             Money: $0
+#         Report after purchasing latte:
+#             Water: 100ml
+#             Milk: 50ml
+#             Coffee: 76g
+#             Money: $2.5
+#     b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If latte was their choice of drink.
