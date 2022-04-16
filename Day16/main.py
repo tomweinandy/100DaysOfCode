3e7400b17fbabd35b5b4112b7833c34e1a4383e1
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-16 08:06:38 -0400

    Prompt user for a drink order

--- Day16/main.py
+++ Day16/main.py
@@ -1,14 +1,29 @@
+"""
+Coffee Machine Program (Pt II)
+- This replicates the
+"""
+
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-latte = MenuItem(
-    name='latte',
-    cost=2.5,
-    water=200,
-    milk=150,
-    coffee=24
-)
+# latte = MenuItem(
+#     name='latte',
+#     cost=2.5,
+#     water=200,
+#     milk=150,
+#     coffee=24
+# )
 
-menu = Menu([latte])
+menu = Menu()
 
+# Separate drinks into list (removing last item, which is empty)
+menu_list = menu.get_items().split('/')[:-1]
+
+# Force correct drink order from list
+order = ''
+while order not in menu_list:
+    order = input(f'What would you like? ({menu.get_items()}): ')
+
+
+print(menu_list)
