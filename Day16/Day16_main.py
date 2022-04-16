b3d968e4c1aa5de00cc709ae7999833df6fefe18
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-16 08:58:57 -0400

    Final comments

--- Day16/Day16_main.py
+++ Day16/Day16_main.py
@@ -1,19 +1,14 @@
 """
 Coffee Machine Program (Pt II)
-- This replicates the
+- This replicates the program from Day 15 but uses a class structure approach
+- Only this file was created by me--the rest came from course resources
+- See the pdf of documentation for details
 """
 
-from Day16_menu import Menu, MenuItem
+from Day16_menu import Menu
 from Day16_coffee_maker import CoffeeMaker
 from Day16_money_machine import MoneyMachine
 
-# latte = MenuItem(
-#     name='latte',
-#     cost=2.5,
-#     water=200,
-#     milk=150,
-#     coffee=24
-# )
 
 # Initialize class objects
 menu = Menu()
@@ -22,19 +17,15 @@ money_machine = MoneyMachine()
 
 
 # Set initial conditions
-# order = ''
 menu_list = menu.get_items().split('/')[:-1]  # Separate drinks into list (removing last item, which is empty)
-input_list = menu_list + ['report', 'off']  # Add additional functions to input
-# # drink_list = [k for k in menu.keys()]
-# ingredient_list = [k for k in resources.keys()]
-# resources['money'] = 0
-# menu['espresso']['ingredients']['milk'] = 0
-powered_on = True
+input_list = menu_list + ['report', 'off']    # Add additional functions to input
+powered_on = True                             # You can't make coffee if the machine is off, obviously
 
 
 # Machine operates while powered on
 while powered_on:
 
+    # Place order
     order = input(f'What would you like? ({menu.get_items()}): ')
 
     # Ask again if order is invalid
@@ -46,39 +37,21 @@ while powered_on:
         coffee_maker.report()
         money_machine.report()
 
+    # Check drink is on the menu
     elif order in menu_list:
         drink = menu.find_drink(order)
 
+        # Check sufficient resouces
         if coffee_maker.is_resource_sufficient(drink):
 
+            # Check payment is sufficient
             if money_machine.make_payment(drink.cost):
 
+                # Make the coffee
                 coffee_maker.make_coffee(drink)
 
         else:
             print('Sorry, insufficient ingredients.')
 
-
-
-
-
-
     elif order == 'off':
         powered_on = False
-
-
-
-
-
-# # todo 7. Make Coffee.
-#     a. If the transaction is successful and there are enough resources to make the drink the user selected, then the ingredients to make the drink should be deducted from the coffee machine resources.
-#         E.g. report before purchasing latte: Water: 300ml
-#             Milk: 200ml
-#             Coffee: 100g
-#             Money: $0
-#         Report after purchasing latte:
-#             Water: 100ml
-#             Milk: 50ml
-#             Coffee: 76g
-#             Money: $2.5
-#     b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If latte was their choice of drink.
