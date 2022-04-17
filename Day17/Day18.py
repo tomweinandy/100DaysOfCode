12481a87d2c1831682f65b208e604cfc209cd098
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-17 17:18:24 -0400

    Documented center_python and showed the math

--- Day17/Day18.py
+++ Day17/Day18.py
@@ -39,7 +39,6 @@ def square(t, distance):
     t.forward(distance)
 
 # square(danklin, 100)
-# shimmy()
 
 # Challenge 2: Draw a dashed line
 def dash(t, interval, distance):
@@ -62,13 +61,11 @@ def dashed_square(t, interval, distance):
     dash(t, interval, distance)
     t.right(90)
     dash(t, interval, distance)
-#
+
 # dashed_square(danklin, 10, 100)
-# shimmy()
 
 
 # Challenge 3: Drawing nested shapes
-
 def ploygon(t, sides, side_length, color='red'):
     t.color(color)
     for side in range(sides):
@@ -84,10 +81,32 @@ color_list = ['red', 'orange', 'blue', 'black', 'brown', 'grey', 'yellow', 'pink
 random.shuffle(color_list)
 
 def center_polygon(t, sides, side_length):
-    theta = 180/sides
-    theta_radians = math.radians(theta)
-    hypotenuse = (side_length/2) / math.sin(theta_radians)
+    """
+    * Moves turtle to the west corner of the northernmost side of a polygon.
+    * There are two possible ways of reaching this point.
+      The first way is to travel due north, turn left 90 degrees, and then travel due west.
+      The second way is to turn directly to the corner and travel diagonally (northwest) to the same destination.
+    * Image plotting those two paths together to form a right triangle.
+      Then we can use known measurements to calculate the length of the hypotenuse.
+    * We know the angle between the adjacent triangle side and the hypotenuse as 360 degrees / (2 * the # of sides).
+      E.g., a square has 8 right triangles that pinwheel out in 360 degrees from the centroid (8 = 2 * 4 sides).
+      Therefore, this inner angle for a square is 360/(2*4) = 45 degrees. I call this angle 'theta'.
+    * We also know the length of the opposite side from theta as half the length of the polygon side.
+    * Since sin(theta) = opposite / hypotenuse, we can solve for hypotenuse = (side length / 2) / sin(360/(2*sides)).
+    * Now we turn theta degrees and move a distance equal to the hypotenuse to reach the northwest corner.
+      From here we begin drawing the polygon so that the centroid aligns with the origin of our turtle.
 
+    :param t: the Turtle() class object
+    :param sides: number of sides in an equilateral polygon
+    :param side_length: length of each polygon side
+    :return:
+    """
+    # Define variables
+    theta = 360/(2*sides)
+    sine_theta = math.sin(math.radians(theta))  # math.sin() takes input as radians
+    hypotenuse = (side_length/2) / sine_theta
+
+    # Move turtle
     t.penup()
     t.left(90 + theta)
     t.forward(hypotenuse)
