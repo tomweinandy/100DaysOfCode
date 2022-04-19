6eaae2f35d87ce8de09f4842d825683934b49083
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-19 07:40:44 -0400

    Turtle Race!

--- Day19/Day19_turtle_race.py
+++ Day19/Day19_turtle_race.py
@@ -1,56 +1,50 @@
 """
-Day 19: Etch-A-Sketch
+Day 19: Turtle Race
 """
 import turtle
+import random
 
 # Docs: https://docs.python.org/3/library/turtle.html
 
-
-t = turtle.Turtle()
-t.shape('turtle')
+# Set initial conditions
 screen = turtle.Screen()
+screen.setup(width=1000, height=500)
 
+# Make a turtle for each color and save to a list
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+y = 200
+turtle_list = []
 
-# Challenge 1: Make an Etch-A-Sketch
-# Define action functions
-def move_forward():
-    t.forward(10)
+for color in colors:
+    new_turtle = turtle.Turtle(shape='turtle')
+    new_turtle.penup()
+    new_turtle.color(color)
+    new_turtle.goto(-425, y)
+    y -= 75
+    turtle_list.append(new_turtle)
 
+# Player guesses which turtle will win
+bet = screen.textinput(title='Place your bets!',
+                       prompt='Which color turtle do you think will win? (red/orange/yellow/green/blue/purple): '
+                       ).lower()
 
-def move_backward():
-    t.forward(-10)
+# Begin the race
+race_on = True
+while race_on:
+    for t in turtle_list:
+        random_distance = random.randint(0, 10)
+        t.forward(random_distance)
 
+        # Stop after a turtle reaches the end
+        if t.xcor() > 475:
+            winner = t.color()[0]
+            print(f'The {winner} turtle won!')
+            race_on = False
 
-def turn_right():
-    t.right(10)
-
-
-def turn_left():
-    t.left(10)
-
-
-def clear_screen():
-    t.clear()
-
-
-# "Listens" for keystrokes
-screen.listen()
-
-# Links keystrokes to functions
-screen.onkey(key='w', fun=move_forward)
-screen.onkey(key='s', fun=move_backward)
-screen.onkey(key='a', fun=turn_left)
-screen.onkey(key='d', fun=turn_right)
-screen.onkey(key='c', fun=clear_screen)
-
-
-
-
-
-
-
-
-
+# Check if the bet was correct
+if bet == winner:
+    print('You get all the monies!')
+else:
+    print('And this is why people don\'t bet on turtle races')
 
 screen.exitonclick()
-
