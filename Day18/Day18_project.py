ac9c8947528e6611b598f14f06cb5980a717accd
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-19 06:17:47 -0400

    Added Jackon Pollock style painting to final project

--- Day18/Day18_project.py
+++ Day18/Day18_project.py
@@ -17,7 +17,6 @@ t.color('red')
 t.pensize(2)
 t.speed(10)
 
-
 # Extract colors from the jpg
 colorgram_list = []
 colors = colorgram.extract('kill_bill.jpeg', 8)
@@ -30,30 +29,71 @@ for color in colors:
     tup = (r, g, b)
     colorgram_list.append(tup)
 
+
+# Day 18 Project
 # Position the turtle
-turtle.colormode(255)
-t.penup()
-t.goto(-305, 305)
-t.pendown()
+# turtle.colormode(255)
+# t.penup()
+# t.goto(-305, 305)
+# t.pendown()
+#
+# # Set dots for a 7x7 grid
+# dots = 49
+# for i in range(dots):
+#     # Make a dot
+#     color = random.choice(colorgram_list)
+#     t.dot(50, color)
+#
+#     # Move the turtle
+#     t.penup()
+#     # Move to the next line after every 7th dot is placed
+#     if (i+1) % 7 == 0:
+#         t.right(90)
+#         t.forward(100)
+#         t.right(90)
+#         t.forward(600)
+#         t.right(180)
+#     else:
+#         t.forward(100)
+#     t.pendown()
 
-# Set dots for a 7x7 grid
-dots = 49
-for i in range(dots):
-    # Make a dot
-    color = random.choice(colorgram_list)
-    t.dot(50, color)
 
-    # Move the turtle
+
+# Bonus: Random Walk with extracted colors
+def random_walk(t, steps, step_length, color='random'):
+    """
+    Draws a line according to a random walk
+    :param t: The turtle of interest
+    :param steps: Number of steps in the walk
+    :param step_length: The length of each step in the random walk
+    :param color: Line color in (r,g,b) format from
+    """
+    # Begins at the origin
     t.penup()
-    # Move to the next line after every 7th dot is placed
-    if (i+1) % 7 == 0:
-        t.right(90)
-        t.forward(100)
-        t.right(90)
-        t.forward(600)
-        t.right(180)
-    else:
-        t.forward(100)
+    t.goto(0, 0)
     t.pendown()
+
+    turtle.colormode(255)
+    t.color(color)
+
+    # Random walk
+    for step in range(steps):
+        t.right(random.randint(0, 360))
+        t.forward(step_length)
+
+
+turtle.tracer(0, 0)  # Comment out to watch the turtle draw
+
+# Take a million steps
+t.pensize(5)
+seed = random.randint(0, 1000)
+print('Seed:', seed)
+random.seed(seed)
+for i in range(0, 1000):
+    color = random.choice(colorgram_list)
+    random_walk(t, 1000, 50, color=color)
+turtle.update()
+
+
 
 screen.exitonclick()
