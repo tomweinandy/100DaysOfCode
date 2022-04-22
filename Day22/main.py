3f7200ea9ed0d856f610fc004a51559ff773b539
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-22 10:30:24 -0400

    Began Day 22 project

---
+++ Day22/main.py
@@ -0,0 +1,16 @@
+"""
+Day 22: Pong
+"""
+
+import turtle
+
+screen = turtle.Screen()
+screen.title('Pong')
+screen.setup(width=800, height=600)
+screen.bgcolor('black')
+
+
+
+
+
+screen.exitonclick()
