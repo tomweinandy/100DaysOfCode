0ad57797778f888e26d5d74df0a9b83ae11defb3
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-23 13:57:35 -0400

    Added traffic

--- Day23/car_manager.py
+++ Day23/car_manager.py
@@ -1,7 +1,38 @@
+import random
+from turtle import Turtle
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-STARTING_MOVE_DISTANCE = 5
+STARTING_MOVE_DISTANCE = 20
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class Car(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape('square')
+        self.turtlesize(stretch_len=2)
+        self.color(random.choice(COLORS))
+        self.goto(360, random.randint(-260, 260))
+        # self.backward(STARTING_MOVE_DISTANCE)
+
+    def move(self):
+        new_x = self.xcor() - MOVE_INCREMENT
+        self.goto(new_x, self.ycor())
+
+
+class CarManager(Turtle):
+    def __init__(self, traffic_rate):
+        super().__init__()
+        self.cars = []
+        self.traffic_rate = traffic_rate
+
+    def add_cars(self):
+        if random.random() < self.traffic_rate:
+            self.cars.append(Car())
+
+    def green_light(self):
+        for car in self.cars:
+            car.move()
+
+
+
--- Day23/main.py
+++ Day23/main.py
@@ -8,7 +8,21 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+car_manager = CarManager(1)
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.add_cars()
+    car_manager.green_light()
+
+
+    # todo add turtle movement function
+    # todo detect car collision
+    # todo detect when the turtle crosses the finish line
+    # todo advance to next level and tune car speed
+    #
+
+screen.exitonclick()
