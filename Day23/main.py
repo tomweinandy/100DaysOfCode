5fa51b1ae35d1e812f5298ae4c5bda9c56eb687e
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-23 14:36:28 -0400

    Added collision detection

--- Day23/main.py
+++ Day23/main.py
@@ -10,23 +10,29 @@ screen.tracer(0)
 
 # Initialize player and traffic
 player = Player()
-car_manager = CarManager(0.1)
+car_manager = CarManager(0.5)
 car_manager.hideturtle()
 
 # Add player movement
 screen.listen()
 screen.onkey(player.move, 'Up')
 
-game_is_on = True
-while game_is_on:
+game_on = True
+while game_on:
     time.sleep(0.1)
     screen.update()
 
     car_manager.add_cars()
     car_manager.green_light()
 
-    # todo detect car collision
-    # todo detect when the turtle crosses the finish line
+    for car in car_manager.cars:
+        x_distance = abs(car.position()[0] - player.position()[0])
+        y_distance = abs(car.position()[1] - player.position()[1])
+
+        if x_distance < 5 and y_distance < 20:
+            game_on = False
+
     # todo advance to next level and tune car speed
+    # todo fine tune collision distance
 
 screen.exitonclick()
