4ef4ed5be037caf2b0e2da103ee36ae1047c5975
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-25 14:11:43 -0400

    Documented solution

--- Day25/main.py
+++ Day25/main.py
@@ -6,6 +6,11 @@ import pandas as pd
 
 
 def print_state(data, state):
+    """
+    Write correct guesses to the map
+    :param data: dataframe of U.S. states
+    :param state: the guessed state
+    """
     answer_data = data[data['state'] == state]
     answer_idx = answer_data.index[0]
     x = answer_data['x'][answer_idx]
@@ -18,44 +23,47 @@ def print_state(data, state):
     t.goto(1000, 1000)
 
 
+# Initialize screen, adding title and map
 screen = turtle.Screen()
 screen.title('U.S. States Game')
 image = 'blank_states_img.gif'
 screen.addshape(image)
-screen.tracer(0)
-# screen.update()
 turtle.shape(image)
+screen.tracer(0)
 
+# Add state data, including new column where correct guesses will be added
 df = pd.read_csv('50_states.csv')
-
 df['guessed'] = 0
 
-print(df.head())
-
+# While game is True, let the player make a quess
 game_on = True
 while game_on:
-
     screen.update()
 
+    # Show remaining states to guess as the title of the input box
     correct_guesses = sum(df['guessed'])
     title_str = f'{correct_guesses}/50 States Correct'
+
+    # Take input, making lowercase then making titlecase
     answer_state = screen.textinput(title=title_str, prompt='What\'s another state\'s name?')
     answer_state = answer_state.lower().title()
 
+    # Filter dataframe to only include row with guessed state (returns 0 rows if guessed state does not exist)
     answer_df = df[df['state'] == answer_state]
-    print(answer_df)
 
+    # If guess matches a state (otherwise answer_df is empty with length 0)
     if len(answer_df) > 0:
         correct_answer_index = answer_df.index[0]
         df.at[correct_answer_index, 'guessed'] = 1
-        print_state(df, answer_state)
+        print(f'{answer_state} is a match!')
+    else:
+        print(f'"{answer_state}" does not exist (check your spelling)')
 
+    # Game ends when player types 'Quit' or guesses all 50 states
     if answer_state == 'Quit' or correct_guesses == 50:
         game_on = False
         break
 
     screen.update()
-
-
 
 screen.exitonclick()
