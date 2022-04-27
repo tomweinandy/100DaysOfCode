6d42f4fb205498ad2e2eb33fd4a40e2ce8163be5
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-27 09:25:30 -0400

    Finished Day 27 MVP

--- Day27/Day27.py
+++ Day27/Day27.py
@@ -1,36 +1,49 @@
 """
-Day 27
+Day 27: GUI Miles to Kilometers Converter
 """
 import tkinter
 
+LABEL_FONT = ('Arial', 24)
+
 
 def button_clicked():
-    print('I got clicked')
-    new_text = input.get()
-    my_label['text'] = new_text
+    miles = int(entry.get())
+    km = miles*1.60934
+    km = round(km, 3)
+    label3['text'] = km
+    print(f'{miles} mile(s) is equal to {km} kilometers')
 
 
 window = tkinter.Tk()
-window.title('My First GUI Interface')
+window.title('Mile to Kilometer Converter')
 window.minsize(width=500, height=300)
-window.config(padx=50, pady=50)
+window.config(padx=100, pady=100)
 
-# Label
-my_label = tkinter.Label(text='I am a label', font=('Arial', 24, 'bold'))
-my_label.grid(column=0, row=0)
+# Label1
+label1 = tkinter.Label(text='Mile(s)', font=LABEL_FONT)
+label1.grid(row=0, column=2)
+
+# Label2
+label2 = tkinter.Label(text='is equal to', font=LABEL_FONT)
+label2.grid(row=1, column=0)
+
+# Label3
+label3 = tkinter.Label(text='0', font=LABEL_FONT)
+label3.grid(row=1, column=1)
+
+# Label4
+label4 = tkinter.Label(text='Km', font=LABEL_FONT)
+label4.grid(row=1, column=2)
 
 # Button
-button = tkinter.Button(text='Click me', command=button_clicked)
-button.grid(column=2, row=0)
-
-# New button
-new_button = tkinter.Button(text='Click me too', command=button_clicked)
-new_button.grid(column=1, row=1)
+button = tkinter.Button(text='Calculate', command=button_clicked, font=LABEL_FONT)
+button.grid(row=2, column=1)
 
 # Entry
-input = tkinter.Entry(width=10)
-print(input.get())
-input.grid(column=3, row=2)
+entry = tkinter.Entry(width=10, font=LABEL_FONT)
+entry.insert(tkinter.END, string='0')
+print(entry.get())
+entry.grid(row=0, column=1)
 
 
 # Keeps window open
