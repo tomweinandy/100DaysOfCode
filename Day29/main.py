6ec992011e3fac1ae8ab4c3c57cfbb93366e85a3
Thomas Weinandy <tomweinandy@gmail.com>
2022-04-30 07:16:33 -0400

    Added checks and popup

--- Day29/main.py
+++ Day29/main.py
@@ -2,11 +2,32 @@
 Day 29: Password Manager
 """
 import tkinter
+from tkinter import messagebox
+import random
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    pass
+    # Password Generator Project
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    list_letters = [random.choice(letters) for letter in range(nr_letters)]
+    list_symbols = [random.choice(symbols) for symbol in range(nr_symbols)]
+    list_numbers = [random.choice(numbers) for number in range(nr_numbers)]
+
+    password_list = list_letters + list_symbols + list_numbers
+    random.shuffle(password_list)
+    password = ''.join(password_list)
+
+    print(f"Your password is: {password}")
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -17,15 +38,25 @@ def add_password():
     password = password_entry.get()
     new_entry = f'{website} | {username} | {password}\n'
 
-    # Open txt file of passwords
-    with open('data.txt', 'a') as file:
-        file.write(new_entry)
+    # Ask user with pop-up if info correct
+    is_ok = messagebox.askokcancel(message=f'Website: {website} \nEmail/Username: {username}'
+                                           f'\nPassword: {password} \nIs it okay to save?')
 
-    # Delete text in entry
-    website_entry.delete(0, tkinter.END)
-    password_entry.delete(0, tkinter.END)
+    # Check for non-empty entries
+    if len(website) > 0 and len(username) > 0 and len(password) > 0:
 
+        # If two checks pass
+        if is_ok:
+            # Open txt file of passwords
+            with open('data.txt', 'a') as file:
+                file.write(new_entry)
 
+            # Delete text in entry (from index 0 to end)
+            website_entry.delete(0, tkinter.END)
+            password_entry.delete(0, tkinter.END)
+
+    else:
+        messagebox.showerror(title='Invalid Entry', message='You may not have an empty entry.')
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Initialize window
