63c85c2dcef4ad876bf33f19551b392b4ff225dd
Thomas Weinandy <tomweinandy@gmail.com>
2022-05-01 10:25:02 -0400

    Created new flash card with click

--- Day31/main.py
+++ Day31/main.py
@@ -1,24 +1,62 @@
 """
-Day 31: Flashcard
+Day 31: Flash Card App
 """
 import tkinter
+import pandas as pd
+import random
 
+# Define constant variables
 BACKGROUND_COLOR = "#B1DDC6"
 
 FRONT_IMAGE_PATH = 'images/card_front.png'
 RIGHT_IMAGE_PATH = 'images/right.png'
 WRONG_IMAGE_PATH = 'images/wrong.png'
+DATA_PATH = 'data/french_words.csv'
+
+FRONT_TITLE_TEXT = 'French'
+BACK_TITLE_TEXT = 'English'
 
 TITLE_TEXT_FONT = ('Ariel', 40, 'italic')
-WORD_TEXT_FONT = ('Ariel', 60, 'bold')
+ITEM_TEXT_FONT = ('Ariel', 60, 'bold')
+
+
+# Set definitions
+def add_data(data_path):
+    data = pd.read_csv(data_path)
+    data['Status'] = 'Unknown'
+    return data
+
+
+def select_random_unknown_item(data):
+    front_card_col = df.columns[0]
+    back_card_col = df.columns[1]
+    data_unknown = data[data['Status'] == 'Unknown']
+
+    random_idx = random.randint(0, len(data_unknown)-1)
+    random_item = df.iloc[random_idx]
+
+    random_front_item = random_item[front_card_col]
+    random_back_item = random_item[back_card_col]
+
+    return random_idx, random_front_item, random_back_item
 
 
 def mark_right():
-    pass
+    idx, front_item, back_item = select_random_unknown_item(df)
+    canvas.itemconfig(front_item_text, text=front_item)
 
 
 def mark_wrong():
-    pass
+    idx, front_item, back_item = select_random_unknown_item(df)
+    canvas.itemconfig(front_item_text, text=front_item)
+
+
+# Read in dataset
+df = add_data(DATA_PATH)
+idx, front_item, back_item = select_random_unknown_item(df)
+print(idx)
+print(front_item)
+print(back_item)
 
 
 # Initialize window and canvas
@@ -34,6 +72,10 @@ front_image = tkinter.PhotoImage(file=FRONT_IMAGE_PATH)
 canvas.create_image(400, 263, image=front_image)
 canvas.grid(row=0, column=0, columnspan=2)
 
+# Add text
+front_title_text = canvas.create_text(400, 150, text=FRONT_TITLE_TEXT, font=TITLE_TEXT_FONT)
+front_item_text = canvas.create_text(400, 263, text=front_item, font=ITEM_TEXT_FONT)
+
 # Add buttons
 wrong_image = tkinter.PhotoImage(file=WRONG_IMAGE_PATH)
 wrong_button = tkinter.Button(image=wrong_image, highlightthickness=0, command=mark_right)
@@ -43,14 +85,7 @@ right_image = tkinter.PhotoImage(file=RIGHT_IMAGE_PATH)
 right_button = tkinter.Button(image=right_image, highlightthickness=0, command=mark_right)
 right_button.grid(row=1, column=1)
 
-# Add text
-title_label = tkinter.Label(text='French', justify='center', font=TITLE_TEXT_FONT)
-title_label.place(x=330, y=150)
 
-word = 'Word'
-x_offset = 380 - 13.5*len(word)  # offset a function of word length
-title_label = tkinter.Label(text=word, justify='center', font=WORD_TEXT_FONT)
-title_label.place(x=x_offset, y=263)
 
 
 
