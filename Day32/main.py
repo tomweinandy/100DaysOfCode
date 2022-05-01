de9dbca3adf7bebb6d075e99fdab6f2129b4ae95
Thomas Weinandy <tomweinandy@gmail.com>
2022-05-01 16:32:05 -0400

    Added functions to send email to birthday person

--- Day32/main.py
+++ Day32/main.py
@@ -8,47 +8,44 @@ import datetime as dt
 import random
 import pandas as pd
 
+# Define variables
+my_username = 'ignorethistest2022'
+my_domain = '@gmail.com'
+my_email = my_username + my_domain
 
-def write_letter(birthday_person):
+# Add password (dummy account)
+file_path = 'not_very_secure.txt'
+with open(file_path) as file:
+    my_password = file.read()
+
+
+# Add helper functions
+def write_letter(birthday_name):
     # Select a random letter
     random_number = random.randint(1, 3)
     with open(f'letter_templates/letter_{random_number}.txt') as file:
-        letter = file.read()
+        new_letter = file.read()
 
     # Personalize letter
-    letter = letter.replace('[NAME]', birthday_person)
-    letter = letter.replace('Angela', 'Thomas')
+    new_letter = new_letter.replace('[NAME]', birthday_name)
+    new_letter = new_letter.replace('Angela', 'Thomas')
 
-    return letter
+    return new_letter
 
-#
-# # Add email (split up so the bots can't find me)
-# dummy_username = 'ignorethistest2022'
-# dummy_domain = '@gmail.com'
-# dummy_email = dummy_username + dummy_domain
-#
-# my_username = 'tomweinandy'
-# my_domain = '@gmail.com'
-# my_email = my_username + my_domain
-#
-# # Add password (dummy account)
-# file_path = 'not_very_secure.txt'
-# with open(file_path) as file:
-#     dummy_password = file.read()
-#
-# # Had to reduce security level detailed here:
-# # https://www.udemy.com/course/100-days-of-code/learn/lecture/21712834#questions/13766454
-#
-# # Set up SMTP connection
-# # Gmail is smtp.gmail.com, Hotmail is smtp.live.com, Yahoo is smtp.mail.yahoo.com
-# with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
-#     # Start Transfer Layer Security encryption
-#     connection.starttls()
-#     connection.login(user=dummy_email, password=dummy_password)
-#     connection.sendmail(from_addr=dummy_email,
-#                         to_addrs=my_email,
-#                         msg='Subject: Subject line\n\nThis is the body of the email.'
-#                         )
+
+def send_email(birthday_email, birthday_letter):
+# Had to reduce security level detailed here:
+# https://www.udemy.com/course/100-days-of-code/learn/lecture/21712834#questions/13766454
+
+    # Set up SMTP connection
+    # Gmail is smtp.gmail.com, Hotmail is smtp.live.com, Yahoo is smtp.mail.yahoo.com
+    with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+        # Start Transfer Layer Security encryption
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=birthday_email,
+                            msg=f'Subject: Happy Birthday!\n\n{birthday_letter}')
 
 now = dt.datetime.now()
 
@@ -57,20 +54,14 @@ birthdays = pd.read_csv('birthdays.csv')
 for idx, person in birthdays.iterrows():
 
     # Check if birthday is today
-    if not person.day == now.day and not person.month == now.month:
+    if person.day == now.day and person.month == now.month:
         # Write a letter
         letter = write_letter(person['name'])
         print(letter)
 
+        # Email the birthday person
+        send_email(person.email, letter)
+
     else:
-        print('No match today.')
-
-
-# todo 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
-
-
+        print(f'Today is not {person["name"]}\'s birthday.')
 
