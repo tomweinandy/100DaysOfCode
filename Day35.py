68e56eaf749c29e97e4f0db97fbb7aa9043f1678
Thomas Weinandy <tomweinandy@gmail.com>
2022-05-03 10:21:56 -0400

    Resolved issue with creds

--- Day35.py
+++ Day35.py
@@ -12,8 +12,8 @@ EXCLUDE = 'current,minutely,daily,alerts'
 KZOO_LAT = 42.291707
 KZOO_LONG = -85.5872286
 OW_KEY = 'bb3d90c7270f80ab31f92b04ac03926c'
-ACCOUNT_SID = 'ACfcebc83853ac47dce4bf328af90994ac'
-AUTH_TOKEN = 'ACd33664c2011667f5609c2c403195f392'
+ACCOUNT_SID = 'ACfcebc83853ac47dce4bf328af90994ac'  # use live creds
+AUTH_TOKEN = '96ae07df7a7199726b06a7491630c79e'     # use live creds
 PHONE_NUMBER = '+13254137458'
 
 # Get my phone number (saved locally)
@@ -23,39 +23,43 @@ with open(file_path) as file:
 
 print(type(my_number))
 
+# Set credentials
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+# # Method for setting environmental variables in the console. See http://twil.io/secure
+# account_sid = os.environ['ACCOUNT_SID']
+# auth_token = os.environ['AUTH_TOKEN']
+# client = Client(account_sid, auth_token)
+
 # Build URL
 url = f'https://api.openweathermap.org/data/2.5/onecall?lat={KZOO_LAT}&lon={KZOO_LONG}&exclude={EXCLUDE}&appid={OW_KEY}'
 print(url)
 
-# # Get results and print status
-# response = requests.get(url)
-# print(response.status_code)
-# response.raise_for_status()
-# weather_data = response.json()
-#
-# # Determine if it will rain in the next 12 hours
-# will_rain = False
-# for i in range(12):
-#     hour_weather_id = weather_data['hourly'][i]['weather'][0]['id']
-#     if hour_weather_id < 700:
-#         will_rain = True
-# if will_rain:
-#     print('Bring an umbrella')
+# Get results and print status
+response = requests.get(url)
+print(response.status_code)
+response.raise_for_status()
+weather_data = response.json()
 
-# Set the environment variables. See http://twil.io/secure
-# account_sid = os.environ['ACCOUNT_SID']
-# auth_token = os.environ['AUTH_TOKEN']
-# client = Client(account_sid, auth_token)
-client = Client(ACCOUNT_SID, AUTH_TOKEN)
+# Determine if it will rain in the next 12 hours
+will_rain = False
+for i in range(12):
+    hour_weather_id = weather_data['hourly'][i]['weather'][0]['id']
+    if hour_weather_id < 700:
+        will_rain = True
 
-message = client.messages \
-                .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_=PHONE_NUMBER,
-                     to=my_number
-                 )
+if will_rain:
+    print('Bring an umbrella')
 
-print(message.status)
+    # Send message
+    message = client.messages \
+                    .create(
+                         body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                         from_=PHONE_NUMBER,
+                         to=my_number
+                     )
+
+    print(message.status)
 
 
 
