3891205e3fed2486590def3210ab05b02294547f
Thomas Weinandy <tomweinandy@gmail.com>
2022-05-11 22:13:39 -0400

    Finished Step 2

--- Day46/main.py
+++ Day46/main.py
@@ -1,12 +1,10 @@
 """
 Day 46: Musical Time Machine
 """
-# import top_100
+from top_100 import input_date
 import ast
+import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-
-
-# Load and save credentials
 
 # Read in credential string and save as a dictionary
 with open('../../../Dropbox/100DaysOfCodePRIVATE/Day46Creds.txt') as file:
@@ -16,40 +14,25 @@ with open('../../../Dropbox/100DaysOfCodePRIVATE/Day46Creds.txt') as file:
 # Save tokens as variables
 SPOTIPY_CLIENT_ID = creds['SPOTIPY_CLIENT_ID']
 SPOTIPY_CLIENT_SECRET = creds['SPOTIPY_CLIENT_SECRET']
-SPOTIPY_REDIRECT_URI = 'http://example.com'
 
-auth = SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI)
+auth = SpotifyOAuth(
+    scope='playlist-modify-private',
+    client_id=SPOTIPY_CLIENT_ID,
+    client_secret=SPOTIPY_CLIENT_SECRET,
+    redirect_uri='http://example.com',
+    show_dialog=True
+)
 
-auth.get_auth_response()
+sp = spotipy.Spotify(auth_manager=auth)
 
-# # Creates a playlist for a user
-#
-# import argparse
-# import logging
-#
-# import spotipy
-# from spotipy.oauth2 import SpotifyOAuth
-#
-# logger = logging.getLogger('examples.create_playlist')
-# logging.basicConfig(level='DEBUG')
-#
-#
-# def get_args():
-#     parser = argparse.ArgumentParser(description='Creates a playlist for user')
-#     parser.add_argument('-p', '--playlist', required=True,
-#                         help='Name of Playlist')
-#     parser.add_argument('-d', '--description', required=False, default='',
-#                         help='Description of Playlist')
-#     return parser.parse_args()
-#
-#
-# def main():
-#     args = get_args()
-#     scope = "playlist-modify-public"
-#     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-#     user_id = sp.me()['id']
-#     sp.user_playlist_create(user_id, args.playlist)
-#
-#
-# if __name__ == '__main__':
-#     main()
+print(sp.current_user()['id'])
+
+
+# Creates a playlist for a user
+playlist = sp.user_playlist_create(
+                user=sp.current_user()['id'],
+                name=f'Top100--{input_date}',
+                description=f'Top 100 Songs (if available) from {input_date}. See source code here https://github.com/tomweinandy/100DaysOfCode/tree/master/Day46'
+            )
+
+# todo Troubleshoot Step 3 from here
