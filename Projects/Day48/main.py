3683d4a014edef91718fc7f695ecd137b25990a2
Thomas Weinandy <tomweinandy@gmail.com>
2022-05-16 22:29:34 -0400

    Opened test web page with Selenium

--- Projects/Day48/main.py
+++ Projects/Day48/main.py
@@ -1,3 +1,26 @@
 """
 Day 48
+
+Requires download from https://chromedriver.chromium.org/downloads
 """
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+# # Deprecated method
+# chrome_driver_path = '/Applications/chromedriver'
+# driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+chrome_driver_path = Service('/Applications/chromedriver')
+driver = webdriver.Chrome(service=chrome_driver_path)
+#
+# driver.get(product_url)
+# price = driver.find_element('priceblock_ourprice')
+# print(price.text)
+# print(driver.find_element())
+
+# time.sleep(20)
+
+# Close the web page
+# driver.close()
+driver.quit()
+
