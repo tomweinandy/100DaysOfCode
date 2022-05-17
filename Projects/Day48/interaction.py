811f61450dd53411e7cbfed708e67db404327dd0
Thomas Weinandy <tomweinandy@gmail.com>
2022-05-17 11:31:00 -0400

    Began final project

--- Projects/Day48/interaction.py
+++ Projects/Day48/interaction.py
@@ -1,16 +1,45 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.common.keys import Keys
+import time
 
 chrome_driver_path = Service('/Applications/chromedriver')
 driver = webdriver.Chrome(service=chrome_driver_path)
 
-url = 'https://en.wikipedia.org/wiki/Main_Page'
-driver.get(url)
+# url = 'https://en.wikipedia.org/wiki/Main_Page'
+# driver.get(url)
 
-xpath = '//*[@id="articlecount"]/a[1]'
-n = driver.find_element(by=By.XPATH, value=xpath)
-print(n.text)
+# # Click article count
+# css = '#articlecount a'
+# article_count = driver.find_element(by=By.CSS_SELECTOR, value=css)
+# print(article_count.text)
+# article_count.click()
 
-driver.close()
+# # Click another link
+# content_portals = driver.find_element(by=By.LINK_TEXT, value='Content portals')
+# content_portals.click()
+
+# # Use search bar
+# search_bar = driver.find_element(by=By.CLASS_NAME, value='vector-search-box-input')
+# search_bar.send_keys('Python')
+# search_bar.send_keys(Keys.ENTER)
+
+# Fill out form
+lab_report_url = 'https://secure-retreat-92358.herokuapp.com/'
+driver.get(lab_report_url)
+
+first_name = driver.find_element(by=By.NAME, value='fName')
+first_name.send_keys('Thomas')
+
+last_name = driver.find_element(by=By.NAME, value='lName')
+last_name.send_keys('Weinandy')
+
+email = driver.find_element(by=By.NAME, value='email')
+email.send_keys('test@email.com')
+
+sign_up = driver.find_element(by=By.CSS_SELECTOR, value='button')
+sign_up.send_keys(Keys.ENTER)
+
+time.sleep(10)
+driver.quit()
--- Projects/Day48/main.py
+++ Projects/Day48/main.py
@@ -6,40 +6,31 @@ Requires download from https://chromedriver.chromium.org/downloads
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-
-# # Deprecated method
-# chrome_driver_path = '/Applications/chromedriver'
-# driver = webdriver.Chrome(executable_path=chrome_driver_path)
+from selenium.webdriver.common.keys import Keys
+import time
 
 chrome_driver_path = Service('/Applications/chromedriver')
 driver = webdriver.Chrome(service=chrome_driver_path)
 
-
-product_url = 'https://www.amazon.com/KitchenAid-KSM150PSER-Artisan-Tilt-Head-Pouring/dp/B00005UP2P/ref=sr_1_3' \
-            '?crid=3W0N492X9XD4I&keywords=kitchenaid%2Bmixer' \
-            '&qid=1652645519&s=home-garden' \
-            '&sprefix=kitchen%2Cgarden%2C272&sr=1-3&th=1'
-
-url = 'https://www.python.org/'
+url = 'https://orteil.dashnet.org/cookieclicker/'
 driver.get(url)
+time.sleep(30)
 
-events_dict = {}
+xpath = '//*[@id="bigCookie"]'
+big_cookie = driver.find_element(by=By.XPATH, value=xpath)
 
-for i in range(1, 6):
-    xpath_date = f'//*[@id="content"]/div/section/div[3]/div[2]/div/ul/li[{i}]/time'
-    date = driver.find_element(by=By.XPATH, value=xpath_date).text
-    xpath_event = f'//*[@id="content"]/div/section/div[3]/div[2]/div/ul/li[{i}]/a'
-    event = driver.find_element(by=By.XPATH, value=xpath_event).text
+# Click cookie
+for i in range(100):
+    big_cookie.click()
 
-    try:
-        events_dict[date].append(event)
-    except KeyError:
-        events_dict[date] = [event]
+# Buy cursor
+cursor = driver.find_element(by=By.ID, value='product0')
+cursor.click()
 
-print(events_dict)
-# time.sleep(20)
+
+# time.sleep(100)
 
 # Close the web page
-driver.close()
+# driver.close()
 # driver.quit()
 
