aa0380556e5881bd5b3391e6e6dcf3399095e975
Thomas Weinandy <tomweinandy@gmail.com>
2022-05-18 07:49:38 -0400

    Completed Day 48

--- Projects/Day48/main.py
+++ Projects/Day48/main.py
@@ -6,31 +6,53 @@ Requires download from https://chromedriver.chromium.org/downloads
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
 import time
 
+# Set up driver
 chrome_driver_path = Service('/Applications/chromedriver')
 driver = webdriver.Chrome(service=chrome_driver_path)
 
 url = 'https://orteil.dashnet.org/cookieclicker/'
 driver.get(url)
-time.sleep(30)
+time.sleep(10)
 
+# Find the big cookie
 xpath = '//*[@id="bigCookie"]'
 big_cookie = driver.find_element(by=By.XPATH, value=xpath)
 
 # Click cookie
-for i in range(100):
+total_clicks = 10**6
+for i in range(total_clicks):
     big_cookie.click()
 
-# Buy cursor
-cursor = driver.find_element(by=By.ID, value='product0')
-cursor.click()
+    # Check every 200 clicks
+    if i % 200 == 0:
+        # Try to buy the first upgrade
+        try:
+            upgrade = driver.find_element(by=By.ID, value='upgrade0')
+            upgrade.click()
+        except:
+            pass
 
+        # Try to click the golden cookie
+        try:
+            golden_cookie = driver.find_element(by=By.CLASS_NAME, value='shimmer')
+            golden_cookie.click()
+        except:
+            pass
 
-# time.sleep(100)
+    # Check every 2000 clicks
+    if i % 2000 == 0:
+        # Buy buildings
+        for i in range(20, -1, -1):
+            remaining = round(100*i/total_clicks, 2)
+            print(f'{remaining}% done')
+            try:
+                building = driver.find_element(by=By.ID, value=f'product{i}')
+                building.click()
+            except:
+                pass
 
 # Close the web page
-# driver.close()
 # driver.quit()
 
