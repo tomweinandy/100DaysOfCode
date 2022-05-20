db35d1cc0204a1bbe880f5a53aefc81082be5548
Thomas Weinandy <tomweinandy@gmail.com>
2022-05-20 10:50:10 -0400

    Ran internet speed check

--- Projects/Day51/Day51.py
+++ Projects/Day51/Day51.py
@@ -5,6 +5,10 @@ Requires download from https://chromedriver.chromium.org/downloads
 """
 import json
 import TwitterBot
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
 # Load and set credentials
 with open('../../../../Dropbox/100DaysOfCodePRIVATE/Day51Creds.json') as file:
@@ -16,5 +20,8 @@ INTERNET_USERNAME = creds['INTERNET_USERNAME']
 INTERNET_PASSWORD = creds['INTERNET_PASSWORD']
 
 bot = TwitterBot.InternetSpeedTwitterBot()
+print(bot.get_internet_speed()[0])
 
-bot
+
+print('checkpoint 7')
+
