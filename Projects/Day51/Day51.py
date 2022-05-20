0e2edef53443466e8c881c9fd0901af15741280e
Thomas Weinandy <tomweinandy@gmail.com>
2022-05-20 12:15:45 -0400

    Checkpoint

--- Projects/Day51/Day51.py
+++ Projects/Day51/Day51.py
@@ -16,12 +16,14 @@ with open('../../../../Dropbox/100DaysOfCodePRIVATE/Day51Creds.json') as file:
 
 TWITTER_USERNAME = creds['TWITTER_USERNAME']
 TWITTER_PASSWORD = creds['TWITTER_PASSWORD']
-INTERNET_USERNAME = creds['INTERNET_USERNAME']
-INTERNET_PASSWORD = creds['INTERNET_PASSWORD']
 
 bot = TwitterBot.InternetSpeedTwitterBot()
-print(bot.get_internet_speed()[0])
-
-
+upload_speed = bot.get_internet_speed()[0]
+print()
 print('checkpoint 7')
+
+message = f'Dear internet provider, my upload speed is only {upload_speed}Mbps!'
+bot.tweet_at_provider(TWITTER_USERNAME, TWITTER_PASSWORD, message)
+
+print('checkpoint X')
 
