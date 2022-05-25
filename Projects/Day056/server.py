cbc4d7ef43d6bfa5f4367f03e9d09cdf84f9d06e
Thomas Weinandy <tomweinandy@gmail.com>
2022-05-25 11:14:27 -0400

    Added html template

--- Projects/Day056/server.py
+++ Projects/Day056/server.py
@@ -1,3 +1,18 @@
 """
 Day 56:
 """
+from flask import Flask, render_template
+
+# Add name of current directory
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
