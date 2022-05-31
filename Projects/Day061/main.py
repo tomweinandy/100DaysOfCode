c81f61f542f5a7a350a9b9cc8d7bf4603f774aa2
Thomas Weinandy <tomweinandy@gmail.com>
2022-05-31 10:08:29 -0400

    Added validation and rendering for success/denied pages

--- Projects/Day061/main.py
+++ Projects/Day061/main.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 import json
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from email_validator import validate_email, EmailNotValidError
 
 app = Flask(__name__)
 
@@ -13,8 +14,10 @@ app.secret_key = creds['SECRET']
 
 
 class MyForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[Email()])
+    too_short = 'Field myst be at least 8 characters long.'
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label='Log In')
 
 
 @app.route("/")
@@ -22,9 +25,20 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=['POST', 'GET'])
 def login():
     form = MyForm()
+    form.validate_on_submit()
+
+    if form.validate_on_submit():
+        print(form.email.data)
+        print(form.password.data)
+
+        if form.email.data == 'admin@email.com' and form.password.data == '12345678':
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+
     return render_template('login.html', form=form)
 
 
