802227e6eb8936f24a9033fb66f648d3c9ce4586
Thomas Weinandy <tomweinandy@gmail.com>
2022-06-11 22:54:59 +0100

    Commented scoreboard

--- Projects/Day023/scoreboard.py
+++ Projects/Day023/scoreboard.py
@@ -10,6 +10,9 @@ TRAFFIC_POSITION = (-280, -280)
 
 class Scoreboard(Turtle):
     def __init__(self):
+        """
+        Use the turtle superclass to create a scoreboard module that keeps track of levels.
+        """
         super().__init__()
         self.level = 1
         self.penup()
@@ -18,12 +21,20 @@ class Scoreboard(Turtle):
         self.write('Turtles never look back', font=FONT)
 
     def level_up(self, traffic_rate):
+        """
+        Increase the level by one
+        :param traffic_rate: The rate of traffic flow (begins at 0.1)
+        """
         self.level += 1
         self.clear()
         self.write_level()
         self.write_traffic(traffic_rate)
 
     def game_over(self, traffic_rate):
+        """
+        Ends the game and prints "game over"
+        :param traffic_rate: The rate of traffic flow (begins at 0.1)
+        """
         self.clear()
         self.write_level()
         self.write_traffic(traffic_rate)
@@ -31,9 +42,16 @@ class Scoreboard(Turtle):
         self.write('Game Over.', font=FONT)
 
     def write_traffic(self, traffic_rate):
+        """
+        Posts the current traffic rate
+        :param traffic_rate: The rate of traffic flow (begins at 0.1)
+        """
         self.goto(TRAFFIC_POSITION)
         self.write(f'Traffic Rate: {traffic_rate}', font=FONT)
 
     def write_level(self):
+        """
+        Posts the current level
+        """
         self.goto(SCOREBOARD_POSITION)
         self.write(f'Level: {self.level}', font=FONT)
