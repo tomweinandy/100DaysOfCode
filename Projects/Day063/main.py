44fa83dad3e412875a4dfb97fec0af49a1cd12ae
Thomas Weinandy <tomweinandy@gmail.com>
2022-06-16 10:48:47 -0400

    Day 63 MVP

--- Projects/Day063/main.py
+++ Projects/Day063/main.py
@@ -4,7 +4,7 @@ Day 63:
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
@@ -15,6 +15,16 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # silences warnings
 Bootstrap(app)
+
+# ** Code commented out because code is supplanted by SQLAlchemy **
+# # Create a database and cursor
+# db = sqlite3.connect('books-collection.db')
+# cursor = db.cursor()
+#
+# # Create table and add entry
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# cursor.execute("INSERT INTO books VALUES(1, 'Hatchet', 'Gary Paulsen', '⭐⭐⭐⭐⭐')")
+
 
 # Create new database
 db = SQLAlchemy(app)
@@ -28,7 +38,7 @@ class Book(db.Model):
     rating = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
-        return f'<Book {self.title}>'
+        return {"id": self.id, "title": self.title, "author": self.author, "rating": self.rating}
 
 
 db.create_all()
@@ -39,8 +49,8 @@ db.create_all()
 # db.session.add(new_book)
 # db.session.commit()
 
-# # Read all records
-# all_books = db.session.query(Book).all
+# Read all records
+all_books = db.session.query(Book).all()
 
 # # Read a particular book
 # book = Book.query.filter_by(title='Hatchet').first()
@@ -62,33 +72,19 @@ db.create_all()
 # db.session.commit()
 
 
-
-# ** Code commented out because code is supplanted by SQLAlchemy **
-# # Create a database and cursor
-# db = sqlite3.connect('books-collection.db')
-# cursor = db.cursor()
-#
-# # Create table and add entry
-# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
-# cursor.execute("INSERT INTO books VALUES(1, 'Hatchet', 'Gary Paulsen', '⭐⭐⭐⭐⭐')")
-
 # all_books = [{'title': 'Hatchet', 'author': 'Gary Paulsen', 'rating': '⭐⭐⭐⭐⭐'}]
 
 
 class BookForm(FlaskForm):
     title = StringField('Book title', validators=[DataRequired()])
     author = StringField('Book author', validators=[DataRequired()])
-    stars = ['⭐️', '⭐⭐️', '⭐⭐⭐️', '⭐⭐⭐⭐️', '⭐⭐⭐⭐⭐']
-    rating = SelectField('Book rating', choices=stars, validators=[DataRequired()])
+    rating = IntegerField('Book rating', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-    def add_to_dict(self, title, author, rating):
-        book_dict = {'title': title,
-                     'author': author,
-                     'rating': rating}
-
-        all_books.append(book_dict)
-        print(all_books)
+    def add_to_db(self, title, author, rating):
+        new_book = Book(title=title, author=author, rating=rating)
+        db.session.add(new_book)
+        db.session.commit()
 
 
 @app.route('/')
@@ -102,12 +98,11 @@ def add():
     print(len(all_books))
 
     if form.validate_on_submit():
-        form.add_to_dict(form.title.data, form.author.data, form.rating.data)
-        return redirect(url_for('add'))
+        form.add_to_db(form.title.data, form.author.data, form.rating.data)
+        return redirect(url_for('home'))
 
     return render_template('add.html', form=form, all_books=all_books)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
