1515de7219933f52fecc94c49dda82ff4452b326
Thomas Weinandy <tomweinandy@gmail.com>
2022-06-19 11:01:27 -0400

    Deleted record

--- Projects/Day066/main.py
+++ Projects/Day066/main.py
@@ -1,3 +1,8 @@
+"""
+Day 66: Build REST API
+
+Completed documentation: https://documenter.getpostman.com/view/21536847/UzBmMnEC
+"""
 import random
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
@@ -104,6 +109,19 @@ def update_price(cafe_id):
 
 
 ## HTTP DELETE - Delete Record
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api-key")
+    if api_key == "NothingGoldCanStay":
+        cafe = db.session.query(Cafe).get(cafe_id)
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"success": "Successfully deleted the cafe from the database."}), 200
+        else:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+    else:
+        return jsonify(error={"Forbidden": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 403
 
 
 if __name__ == '__main__':
