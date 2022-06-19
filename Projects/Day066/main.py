1637d49a641d3894dc3347d9a0651a211d8e1a5e
Thomas Weinandy <tomweinandy@gmail.com>
2022-06-19 09:39:01 -0400

    Added http get requests for random, all, or location-based queries

--- Projects/Day066/main.py
+++ Projects/Day066/main.py
@@ -1,15 +1,16 @@
+import random
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-##Connect to Database
+# Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-##Cafe TABLE Configuration
+# Café TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -23,6 +24,18 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry; where key is column name and value is column value
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+
+# Query all cafés
+cafes = db.session.query(Cafe).all()
+
 
 @app.route("/")
 def home():
@@ -30,6 +43,26 @@ def home():
     
 
 ## HTTP GET - Read Record
+@app.route("/random", methods=['GET'])
+def get_random():
+    random_cafe = random.choice(cafes)
+    return jsonify(random_cafe.to_dict())
+
+
+@app.route("/all", methods=['GET'])
+def get_all():
+    return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
+
+
+@app.route("/search", methods=['GET'])
+def search():
+    location = request.args.get('loc')
+    all_cafes_in_location = db.session.query(Cafe).filter_by(location=location).all()
+    if all_cafes_in_location:
+        return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes_in_location])
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
+
 
 ## HTTP POST - Create Record
 
