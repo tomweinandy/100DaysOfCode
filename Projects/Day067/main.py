a1423c20fb98084781211374340177a1bee7188d
Thomas Weinandy <tomweinandy@gmail.com>
2022-06-21 10:34:53 -0400

    Got index and new page to work. Added new post form function

---
+++ Projects/Day067/main.py
@@ -0,0 +1,99 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
+
+
+## Delete this code:
+# import requests
+# posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap(app)
+
+# CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+# WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = StringField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+    @staticmethod
+    def add_to_db(title, subtitle, date, author, img_url, body):
+        new_post = BlogPost(title=title, subtitle=subtitle, date=date, author=author, img_url=img_url, body=body)
+        db.session.add(new_post)
+        db.session.commit()
+
+
+@app.route('/')
+def get_all_posts():
+    # posts = db.session.query(BlogPost).all()
+    posts = BlogPost.query.all()
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    # requested_post = None
+    # for blog_post in posts:
+    #     if blog_post["id"] == index:
+    #         requested_post = blog_post
+    # requested_post = db.session.query(BlogPost).get(post_id)
+    requested_post = BlogPost.query.get(post_id)
+    return render_template("post.html", post=requested_post)
+
+
+@app.route("/make-post", methods=['GET', 'POST'])
+def make_post():
+    form = CreatePostForm()
+
+    if form.validate_on_submit():
+        today = date.today().strftime('%B %d, %Y')
+        form.add_to_db(form.title.data, form.subtitle.data, today, form.author.data, form.img_url.data, form.body.data)
+        return redirect(url_for('get_all_posts'))
+
+    return render_template('make-post.html', form=form)
+
+
+@app.route("/edit-post/<int:post_id>")
+def edit_post(post_id):
+    pass
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
