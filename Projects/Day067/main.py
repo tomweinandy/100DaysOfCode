0ded1309b454aa6bed90e535f60db8ef550a27d5
Thomas Weinandy <tomweinandy@gmail.com>
2022-06-21 11:07:53 -0400

    Added ability to edit posts

--- Projects/Day067/main.py
+++ Projects/Day067/main.py
@@ -59,11 +59,6 @@ def get_all_posts():
 
 @app.route("/post/<int:post_id>")
 def show_post(post_id):
-    # requested_post = None
-    # for blog_post in posts:
-    #     if blog_post["id"] == index:
-    #         requested_post = blog_post
-    # requested_post = db.session.query(BlogPost).get(post_id)
     requested_post = BlogPost.query.get(post_id)
     return render_template("post.html", post=requested_post)
 
@@ -80,9 +75,25 @@ def make_post():
     return render_template('make-post.html', form=form)
 
 
-@app.route("/edit-post/<int:post_id>")
+@app.route("/edit-post/<int:post_id>", methods=['GET', 'POST'])
 def edit_post(post_id):
-    pass
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post_id))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
 
 
 @app.route("/about")
