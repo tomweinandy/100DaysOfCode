8dab8dc08582c709147e1f509dde8d18b420094f
Thomas Weinandy <tomweinandy@gmail.com>
2022-06-22 07:59:59 -0400

    Linked registration with secrets and added name

--- Projects/Day068/main.py
+++ Projects/Day068/main.py
@@ -1,3 +1,6 @@
+"""
+Day 68:
+"""
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -10,13 +13,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 ##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-#Line below only required once, when creating DB. 
+
+#Line below only required once, when creating DB.
 # db.create_all()
 
 
@@ -25,8 +30,19 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == "POST":
+        new_user = User(
+            email=request.form.get('email'),
+            name=request.form.get('name'),
+            password=request.form.get('password')
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        print(new_user.name)
+        return render_template("secrets.html", name=new_user.name)
+
     return render_template("register.html")
 
 
@@ -36,8 +52,8 @@ def login():
 
 
 @app.route('/secrets')
-def secrets():
-    return render_template("secrets.html")
+def secrets(name):
+    return render_template("secrets.html", name=name)
 
 
 @app.route('/logout')
