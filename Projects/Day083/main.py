b1251c1b338a0bfc9d1d61bf1f3615a0ec38c186
Thomas Weinandy <tomweinandy@gmail.com>
2022-07-24 10:45:56 -0400

    Define player positions and print board

--- Projects/Day083/main.py
+++ Projects/Day083/main.py
@@ -1,4 +1,29 @@
 """
 Day 83: Tic Tac Toe Game
 """
+from prettytable import PrettyTable, ALL
+# 🙅🙆
+
+def print_board(tic_tac_toe_list: list):
+    """
+    :param tic_tac_toe_list: list of nine tic-tac-toe positions
+    """
+    table = PrettyTable()
+    table.header = False
+    table.hrules = ALL
+    table.padding_width = 3
+
+    table.add_row(tic_tac_toe_list[0:3])
+    table.add_row(tic_tac_toe_list[3:6])
+    table.add_row(tic_tac_toe_list[6:])
+    print(table)
+
+
+
+# Make a list of nine blank entries that will serve as the board
+ttt = [' ']*9
+print_board(ttt)
+
+
+# Identify winning permutations
 
