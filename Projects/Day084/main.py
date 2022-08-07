ef1092ff45c675cd7b8b3d20c90ba1d003e169fc
Thomas Weinandy <tomweinandy@gmail.com>
2022-08-07 13:47:59 -0400

    Combine color and opacity conversion into same loop

--- Projects/Day084/main.py
+++ Projects/Day084/main.py
@@ -5,43 +5,39 @@ Reference:
 https://pypi.org/project/Pillow/
 https://docs.python.org/3/library/tkinter.html
 """
-
 from PIL import Image
-import numpy as np
 
-
-img = Image.open('../../../../Downloads/Eli.png')
+# Load images
+filename = 'Eli.png'
+filename = input('What image in your downloads folder would you like to watermark?: ')
+img = Image.open(f'../../../../Downloads/{filename}')
 logo_black = Image.open('pie.png')
 
-# Convert logo to white
-logo_black = logo_black.convert('RGBA')
-data = np.array(logo_black)                             # "data" is a height x width x 4 numpy array
-red, green, blue, alpha = data.T                        # Temporarily unpack the bands for readability
-black_areas = (red == 0) & (blue == 0) & (green == 0)   # Replace black with white... (leaves alpha values alone...)
-data[..., :-1][black_areas.T] = (255, 255, 255)         # Transpose back needed
-logo_white = Image.fromarray(data)
-
-
-
-opacity_level = 170 # Opaque is 255, input between 0-255
-opacity = 0.67 # Opaque is 255, input between 0-255
-
-logo = logo_white.convert("RGBA")
+# Set opacity
+opacity = 0.65
+logo = logo_black.convert("RGBA")
 datas = logo.getdata()
 
+# Change colors and make semi-transparent
 newData = []
 for item in datas:
     new_opacity = int(item[3]*opacity)
-    newData.append((item[0], item[1], item[2], new_opacity))
+
+    # If rbg is black (0,0,0) then make it white (255, 255, 255) and opaque
+    if item[0:3] == (0, 0, 0):
+        newData.append((255, 255, 255, new_opacity))
+
+    # Otherwise, keep colors but make it transparent
+    else:
+        newData.append((item[0], item[1], item[2], 0))
 
 logo.putdata(newData)
 
-
-
+# Place watermark in the bottom-right corner
 x_placement = img.size[0] - logo.size[0]
 y_placement = img.size[1] - logo.size[1]
 logo_placement = (x_placement, y_placement)
 img.paste(logo, logo_placement, logo)
 img.show()
-# img.save('watermark.png')
+img.save('watermark.png')
 
