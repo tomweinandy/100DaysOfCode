464d3ce10598c5dc57e4331ff0d4c2aeaad50fb8
Thomas Weinandy <tomweinandy@gmail.com>
2022-08-08 07:40:35 -0400

    Create basic UI canvas

--- Projects/Day084/main.py
+++ Projects/Day084/main.py
@@ -5,8 +5,60 @@ Reference:
 https://pypi.org/project/Pillow/
 https://docs.python.org/3/library/tkinter.html
 """
-
+from watermark import watermark
 import tkinter
 
+# Configurations
+# Color pallet and hex codes come from colorhunt.co
+TAN = '#FCF8E8'
+BROWN = '#ECB390'
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Arial"
+WORK_SECONDS = 25 * 60
+SHORT_BREAK_SECONDS = 5 * 60
+LONG_BREAK_SECONDS = 20 * 60
 
+def start_timer():
+    print('start timer')
 
+def reset_clicked():
+    print('reset clicked')
+
+# Create window
+window = tkinter.Tk()
+window.title('Make Your Watermark on the World')
+window.config(padx=100, pady=50, bg=TAN)
+
+# Add label
+label = 'Add Pie Logo'
+timer_label = tkinter.Label(text=label, bg=TAN, fg=BROWN, font=(FONT_NAME, 36))
+timer_label.grid(row=0, column=1)
+
+# Show pie logo
+canvas = tkinter.Canvas(width=376, height=376, bg=TAN, highlightthickness=0)
+pie_img = tkinter.PhotoImage(file='pie.png')
+canvas.create_image(200, 200, image=pie_img)
+canvas.grid(row=1, column=1)
+
+# # Add timer text over tomato
+# timer_text = canvas.create_text(102, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+
+# Add checkmarks
+checkmark = '✅'
+checkmarks = ''
+checks = tkinter.Label(text=checkmarks, bg=YELLOW)
+checks.grid(row=3, column=1)
+
+# Add start button
+start_button = tkinter.Button(text='Start', command=start_timer, font=FONT_NAME)
+start_button.grid(row=2, column=0)
+
+# Add reset button
+start_button = tkinter.Button(text='Reset', command=reset_clicked, font=FONT_NAME)
+start_button.grid(row=2, column=2)
+
+# Keep window open
+window.mainloop()
