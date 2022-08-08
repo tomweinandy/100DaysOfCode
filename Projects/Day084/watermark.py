cb4f6704b457c4fe3d55a4ccb8b9abed2510ab95
Thomas Weinandy <tomweinandy@gmail.com>
2022-08-08 07:24:19 -0400

    Add black/white watermark functionality

--- Projects/Day084/watermark.py
+++ Projects/Day084/watermark.py
@@ -1,40 +1,60 @@
 from PIL import Image
 
-# Load images
-filename = input('What image in your downloads folder would you like to watermark?: ')
-img = Image.open(f'../../../../Downloads/{filename}')
-logo_black = Image.open('pie.png')
 
-# Set opacity
-OPACITY = 0.65
-logo = logo_black.convert("RGBA")
-datas = logo.getdata()
+def add(a, b):
+    return a + b
 
-# Change colors and make semi-transparent
-newData = []
-for item in datas:
-    new_opacity = int(item[3] * OPACITY)
 
-    # If rbg is black (0,0,0) then make it white (255, 255, 255) and opaque
-    if item[0:3] == (0, 0, 0):
-        newData.append((255, 255, 255, new_opacity))
+def watermark(filename, black_or_white='white', opacity=0.65, output_filename='default'):
+    # Load images
+    # filename = input('What image in your downloads folder would you like to watermark?: ')
+    img = Image.open(f'../../../../Downloads/{filename}')
+    logo_black = Image.open('pie.png')
 
-    # Otherwise, keep colors but make it transparent
-    else:
-        newData.append((item[0], item[1], item[2], 0))
+    # Set opacity
+    logo = logo_black.convert("RGBA")
+    datas = logo.getdata()
 
-logo.putdata(newData)
+    # Change colors and make semi-transparent
+    newData = []
+    for item in datas:
+        new_opacity = int(item[3] * opacity)
 
-# Place watermark in the bottom-right corner
-x_placement = img.size[0] - logo.size[0]
-y_placement = img.size[1] - logo.size[1]
-logo_placement = (x_placement, y_placement)
-img.paste(logo, logo_placement, logo)
+        # If 'white' is selected
+        if black_or_white == 'white':
+            # If rbg is black (0,0,0) then make it white (255, 255, 255) and opaque
+            if item[0:3] == (0, 0, 0):
+                newData.append((255, 255, 255, new_opacity))
 
-# Show image
-img.show()
+            # Otherwise, keep colors but make it transparent
+            else:
+                newData.append((item[0], item[1], item[2], 0))
 
-# Save new image in Downloads folder
-name_split = filename.split('.')
-name_watermarked = name_split[0] + '_watermarked.' + name_split[1]
-img.save(f'../../../../Downloads/{name_watermarked}')
+        # Otherwise assume 'black' is selected
+        else:
+            # If rbg is black (0,0,0) then keep it black but make opaque
+            if item[0:3] == (0, 0, 0):
+                newData.append((0, 0, 0, new_opacity))
+
+            # Otherwise, keep colors but make it transparent
+            else:
+                newData.append((item[0], item[1], item[2], 0))
+
+
+    logo.putdata(newData)
+
+    # Place watermark in the bottom-right corner
+    x_placement = img.size[0] - logo.size[0]
+    y_placement = img.size[1] - logo.size[1]
+    logo_placement = (x_placement, y_placement)
+    img.paste(logo, logo_placement, logo)
+
+    # Show image
+    img.show()
+
+    # Save new image in Downloads folder
+    if output_filename == 'default':
+        name_split = filename.split('.')
+        output_filename = name_split[0] + '_watermarked.' + name_split[1]
+
+    img.save(f'../../../../Downloads/{output_filename}')
