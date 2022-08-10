442c6637dd30805daa11d70372a27bc900478847
Thomas Weinandy <tomweinandy@gmail.com>
2022-08-09 21:16:54 -0400

    Reinstate start button and countdown

--- Projects/Day085/main.py
+++ Projects/Day085/main.py
@@ -40,32 +40,34 @@ def start_timer():
     """
     Tracks reps and begins timer according to current rep
     """
-    global reps, checkmarks
-    reps += 1
+    # global reps, checkmarks
+    # reps += 1
 
-    # If reps = 8, 16...
-    if reps % 8 == 0:
-        countdown(100)
-        new_label = 'Break Time'
-        timer_label.config(text=new_label, fg=RED)
+    # # If reps = 8, 16...
+    # if reps % 8 == 0:
+    #     countdown(100)
+    #     new_label = 'Break Time'
+    #     timer_label.config(text=new_label, fg=RED)
+    #
+    #     checkmarks += checkmark + '\n'
+    #     checks.config(text=checkmarks)
 
-        checkmarks += checkmark + '\n'
-        checks.config(text=checkmarks)
-
-    # If reps = 2, 4, 6, 10, 12, 14...
-    elif reps % 2 == 0:
-        countdown(10)
-        new_label = 'Break Time'
-        timer_label.config(text=new_label, fg=PINK)
-
-        checkmarks += checkmark
-        checks.config(text=checkmarks)
+    # # If reps = 2, 4, 6, 10, 12, 14...
+    # elif reps % 2 == 0:
+    #     countdown(10)
+    #     new_label = 'Break Time'
+    #     timer_label.config(text=new_label, fg=PINK)
+    #
+    #     checkmarks += checkmark
+    #     checks.config(text=checkmarks)
 
     # If reps = 1, 3, 5, 7, 9, 11, 13, 15, 17...
-    else:
-        countdown(10)
-        new_label = ' Work Time'
-        timer_label.config(text=new_label, fg=GREEN)
+    # else:
+    countdown(120)
+    # new_label = 'Begin!'
+    # timer_label.config(text=new_label, fg=GREEN)
+
+
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -73,7 +75,6 @@ def countdown(count):
     """
     Changes the countdown timer
     :param count: How many seconds to count down from
-    :return:
     """
     global timer
 
@@ -98,33 +99,38 @@ window.config(padx=10, pady=10, bg=YELLOW)  # bg is 'background'
 
 
 # Add timer label
-label = 'How fast of a typer are you?'  # spaces added to match length of other labels
-timer_label = tkinter.Label(text=label, bg=YELLOW, fg='dark blue', font=(FONT_NAME, 36))
+label = 'Type as much as you can in two minutes.\n' \
+        'Spelling counts but there is no penalty for missed punctuation.\n' \
+        'Press "start" to reveal text and begin.'
+timer_label = tkinter.Label(text=label, bg=YELLOW, fg='dark blue', font=(FONT_NAME, 18))
 timer_label.grid(row=0, column=1)
 
 # # Add tomato
 # # The tkinter canvas widget lets us overlay objects on top of each other
-canvas = tkinter.Canvas(width=1000, height=400, bg=YELLOW, highlightthickness=0)
+canvas = tkinter.Canvas(width=1000, height=75, bg=YELLOW, highlightthickness=0)
 # tomato_img = tkinter.PhotoImage(file='tomato.png')
 # canvas.create_image(100, 112, image=tomato_img)
-canvas.grid(row=0, column=1)
 
 # Add timer text
-timer_text = canvas.create_text(500, 40, text='2:00', fill='dark blue', font=(FONT_NAME, 28))
+timer_text = canvas.create_text(500, 40, text='2:00', fill='dark blue', font=(FONT_NAME, 36))
+# timer_text.grid(row=2, column=1)
+canvas.grid(row=1, column=1)
 
-# Add text
-checkmark = '✅'
-checkmarks = ''
-checks = tkinter.Label(text=checkmarks, bg=YELLOW)
-checks.grid(row=3, column=1)
+# # Add text from story to be copied
+# short_story = story[0:100]
+# story_label = tkinter.Label(text=short_story, bg=YELLOW, fg='dark blue', pady=10, font=(FONT_NAME, 12))
+# story_label.grid(row=3, columnspan=3)
+
+
+# # Add text
+# checkmark = '✅'
+# checkmarks = ''
+# checks = tkinter.Label(text=checkmarks, bg=YELLOW)
+# checks.grid(row=3, column=1)
 
 # Add start button
 start_button = tkinter.Button(text='Start', command=start_timer, font=FONT_NAME)
 start_button.grid(row=2, column=1)
-
-# # Add reset button
-# start_button = tkinter.Button(text='Reset', command=reset_clicked, font=FONT_NAME)
-# start_button.grid(row=2, column=2)
 
 # Add main while loop to keep window open
 window.mainloop()
