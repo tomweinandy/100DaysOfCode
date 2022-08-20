81fbee6f84605f442fa533337057ef3375e76235
Thomas Weinandy <tomweinandy@gmail.com>
2022-08-19 21:27:31 -0400

    Replace with task variables

--- Projects/Day088/main.py
+++ Projects/Day088/main.py
@@ -31,7 +31,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # silences warnings
 db = SQLAlchemy(app)
 
 
-# Create movie table
+# Create task table
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), nullable=False)
@@ -58,79 +58,93 @@ db.create_all()
 # Create form for searching for tasks
 class FindTaskForm(FlaskForm):
     title = StringField("Task Title", validators=[DataRequired()])
+    description = TextAreaField("Task Description", validators=[DataRequired()])
+    urgency = StringField("Task Urgency", validators=[DataRequired()])
+    importance = StringField("Task Importance", validators=[DataRequired()])
+    tags = StringField("Task Tags", validators=[DataRequired()])
+    status = StringField("Task Status", validators=[DataRequired()])
     submit = SubmitField("Add Task")
 
 
 # Create form to rate movies
-class RateMovieForm(FlaskForm):
-    rating = StringField("Your Rating Out of 10 e.g. 7.5")
-    review = TextAreaField("Your Review")
+class RateTaskForm(FlaskForm):
+    title = StringField("Task Name")
+    description = TextAreaField("Task Description")
+    urgency = StringField("Urgency from 1 (low) to 5 (high)")
+    importance = StringField("Importance from 1 (low) to 5 (high)")
+    tags = StringField("Task tags, separated by a comma")
+    status = StringField("'Waiting', 'Progressing', or 'Done'")
     submit = SubmitField("Done")
 
 
 # Create homepage
 @app.route("/")
 def home():
-    all_movies = Movie.query.order_by(Movie.rating).all()
-    for i in range(len(all_movies)):
-        all_movies[i].ranking = len(all_movies) - i
+    all_tasks = Task.query.order_by(Task.urgency).all()
+    for i in range(len(all_tasks)):
+        all_tasks[i].ranking = len(all_tasks) - i
     db.session.commit()
-    return render_template("index.html", movies=all_movies)
+    return render_template("index.html", tasks=all_tasks)
 
 
-# Create page for adding movies
+# Create page for adding tasks
 @app.route("/add", methods=["GET", "POST"])
-def add_movie():
-    form = FindMovieForm()
+def add_task():
+    form = RateTaskForm()
     if form.validate_on_submit():
-        movie_title = form.title.data
+        task_title = form.title.data
+        task_description = form.description.data
+        task_urgency = form.urgency.data
+        task_importance = form.importance.data
+        task_tags = form.tags.data
+        task_status = form.status.data
 
-        # Search for movie details with API
-        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": movie_title})
-        data = response.json()["results"]
-        return render_template("select.html", options=data)
+        # # Search for task details with API
+        # response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": task_title})
+        # data = response.json()["results"]
+        # return render_template("select.html", options=data)
     return render_template("add.html", form=form)
 
 
-# Create page for searching for movies
-@app.route("/find")
-def find_movie():
-    movie_api_id = request.args.get("id")
-    if movie_api_id:
-        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
-        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
-        data = response.json()
-        new_movie = Movie(
-            title=data["title"],
-            year=data["release_date"].split("-")[0],
-            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
-            description=data["overview"]
-        )
-        db.session.add(new_movie)
-        db.session.commit()
-        return redirect(url_for("rate_movie", id=new_movie.id))
+# # Create page for searching for tasks
+# @app.route("/find")
+# def find_task():
+#     task_api_id = request.args.get("id")
+#     if task_api_id:
+#         movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+#         response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+#         data = response.json()
+#         new_movie = Movie(
+#             title=data["title"],
+#             year=data["release_date"].split("-")[0],
+#             img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+#             description=data["overview"]
+#         )
+#         db.session.add(new_movie)
+#         db.session.commit()
+#         return redirect(url_for("rate_movie", id=new_movie.id))
 
 
 # Create page for editing movies
 @app.route("/edit", methods=["GET", "POST"])
-def rate_movie():
-    form = RateMovieForm()
-    movie_id = request.args.get("id")
-    movie = Movie.query.get(movie_id)
+def rate_task():
+    form = RateTaskForm()
+    task_id = request.args.get("id")
+    task = Task.query.get(task_id)
     if form.validate_on_submit():
-        movie.rating = float(form.rating.data)
-        movie.review = form.review.data
+        task.rating = float(form.rating.data)
+        task.review = form.review.data
         db.session.commit()
         return redirect(url_for('home'))
-    return render_template("edit.html", movie=movie, form=form)
+    return render_template("edit.html", task=task, form=form)
 
 
 # Create path for deleting a movie
 @app.route("/delete")
-def delete_movie():
-    movie_id = request.args.get("id")
-    movie = Movie.query.get(movie_id)
-    db.session.delete(movie)
+def delete_task():
+    task_id = request.args.get("id")
+    task = Task.query.get(task_id)
+    db.session.delete(task)
     db.session.commit()
     return redirect(url_for("home"))
 
