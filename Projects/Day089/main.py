e9723b140bffb229ed19e5ea72912563f7df90cc
Thomas Weinandy <tomweinandy@gmail.com>
2022-08-22 10:32:00 -0400

    Set new color pallet

--- Projects/Day089/main.py
+++ Projects/Day089/main.py
@@ -5,16 +5,22 @@ import tkinter
 
 # ---------------------------- VARIABLES ------------------------------- #
 # Color pallet and hex codes come from colorhunt.co
+FONT_NAME = 'Arial'
+NAVY = '#1C3879'
+BLUE = '#607EAA'
+BEIGE = '#EAE3D2'
+SAND = '#F9F5EB'
+
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
 WORK_SECONDS = 25 * 60
 SHORT_BREAK_SECONDS = 5 * 60
 LONG_BREAK_SECONDS = 20 * 60
 
 reps = 0
+session_minutes = 60
 timer = None
 
 
@@ -27,7 +33,7 @@ def reset_clicked():
     reps = 0
     checkmarks = ''
     window.after_cancel(timer)
-    timer_label.config(text='Timer', fg=GREEN)
+    timer_label.config(text='Timer', fg=BLUE)
     canvas.itemconfig(timer_text, text='00:00')
     checks.config(text=checkmarks)
 
@@ -44,7 +50,7 @@ def start_timer():
     if reps % 8 == 0:
         countdown(LONG_BREAK_SECONDS)
         new_label = 'Break Time'
-        timer_label.config(text=new_label, fg=RED)
+        timer_label.config(text=new_label, fg=BLUE)
 
         checkmarks += checkmark + '\n'
         checks.config(text=checkmarks)
@@ -53,7 +59,7 @@ def start_timer():
     elif reps % 2 == 0:
         countdown(SHORT_BREAK_SECONDS)
         new_label = 'Break Time'
-        timer_label.config(text=new_label, fg=PINK)
+        timer_label.config(text=new_label, fg=BLUE)
 
         checkmarks += checkmark
         checks.config(text=checkmarks)
@@ -61,8 +67,8 @@ def start_timer():
     # If reps = 1, 3, 5, 7, 9, 11, 13, 15, 17...
     else:
         countdown(WORK_SECONDS)
-        new_label = ' Work Time'
-        timer_label.config(text=new_label, fg=GREEN)
+        new_label = 'Time to Write'
+        timer_label.config(text=new_label, fg=BLUE)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -91,28 +97,25 @@ def countdown(count):
 # Initialize window
 window = tkinter.Tk()
 window.title('Pomodoro')
-window.config(padx=100, pady=50, bg=YELLOW)  # bg is 'background'
+window.config(padx=100, pady=50, bg=SAND)  # bg is 'background'
 
 
 # Add timer label
-label = '  Timer   '  # spaces added to match length of other labels
-timer_label = tkinter.Label(text=label, bg=YELLOW, fg=GREEN, font=(FONT_NAME, 36))
+label = '   Writers UN-block   '  # spaces added to match length of other labels
+timer_label = tkinter.Label(text=label, bg=SAND, fg=BLUE, font=(FONT_NAME, 36))
 timer_label.grid(row=0, column=1)
 
 # Add tomato
 # The tkinter canvas widget lets us overlay objects on top of each other
-canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = tkinter.PhotoImage(file='tomato.png')
-canvas.create_image(100, 112, image=tomato_img)
+canvas = tkinter.Canvas(width=200, height=224, bg=SAND, highlightthickness=0)
+# tomato_img = tkinter.PhotoImage(file='tomato.png')
+# canvas.create_image(100, 112, image=tomato_img)
 canvas.grid(row=1, column=1)
-
-# Add timer text over tomato
-timer_text = canvas.create_text(102, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 
 # Add checkmarks
 checkmark = '✅'
 checkmarks = ''
-checks = tkinter.Label(text=checkmarks, bg=YELLOW)
+checks = tkinter.Label(text=checkmarks, bg=SAND)
 checks.grid(row=3, column=1)
 
 # Add start button
@@ -122,6 +125,12 @@ start_button.grid(row=2, column=0)
 # Add reset button
 start_button = tkinter.Button(text='Reset', command=reset_clicked, font=FONT_NAME)
 start_button.grid(row=2, column=2)
+
+# Add timer text over tomato
+# timer_text = canvas.create_text(-20, 20, text='00:00', fill=BLUE, font=(FONT_NAME, 24, 'bold'))
+timer_text = canvas.create_text(100, 200, text='00:00', fill=BLUE, font=(FONT_NAME, 24, 'bold'))
+# timer_text = tkinter.Label(text='00:00', fg=BLUE, bg=SAND, font=(FONT_NAME, 24, 'bold'))
+# timer_text.grid(row=2, column=1)
 
 # Add main while loop to keep window open
 window.mainloop()
