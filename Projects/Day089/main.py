0037f48e5bc57c18df9e75b7bc40ca73507c8e26
Thomas Weinandy <tomweinandy@gmail.com>
2022-08-22 10:54:10 -0400

    Checkpoint

--- Projects/Day089/main.py
+++ Projects/Day089/main.py
@@ -46,6 +46,11 @@ def start_timer():
     global reps, checkmarks
     reps += 1
 
+    session_length = session_length_entry.get()
+    session_length = int(session_length)
+    #todo split into hours, minutes, seconds
+    print(type(session_length), session_length)
+
     # If reps = 8, 16...
     if reps % 8 == 0:
         countdown(LONG_BREAK_SECONDS)
@@ -118,6 +123,17 @@ checkmarks = ''
 checks = tkinter.Label(text=checkmarks, bg=SAND)
 checks.grid(row=3, column=1)
 
+# Add input label
+input_label = 'Select the number of minutes for this session:'
+timer_label = tkinter.Label(text=input_label, bg=SAND, fg=BLUE, font=(FONT_NAME, 16))
+timer_label.grid(columnspan=2, row=1)
+
+# Adds entry
+session_length_entry = tkinter.Entry(width=10, bg=BEIGE, font=FONT_NAME)
+session_length_entry.insert(tkinter.END, string='0')
+print(session_length_entry.get())
+session_length_entry.grid(row=1, column=2)
+
 # Add start button
 start_button = tkinter.Button(text='Start', command=start_timer, font=FONT_NAME)
 start_button.grid(row=2, column=0)
@@ -126,11 +142,10 @@ start_button.grid(row=2, column=0)
 start_button = tkinter.Button(text='Reset', command=reset_clicked, font=FONT_NAME)
 start_button.grid(row=2, column=2)
 
-# Add timer text over tomato
-# timer_text = canvas.create_text(-20, 20, text='00:00', fill=BLUE, font=(FONT_NAME, 24, 'bold'))
+# Add timer text
 timer_text = canvas.create_text(100, 200, text='00:00', fill=BLUE, font=(FONT_NAME, 24, 'bold'))
-# timer_text = tkinter.Label(text='00:00', fg=BLUE, bg=SAND, font=(FONT_NAME, 24, 'bold'))
-# timer_text.grid(row=2, column=1)
+
+
 
 # Add main while loop to keep window open
 window.mainloop()
