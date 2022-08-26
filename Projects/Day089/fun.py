3588fc537265109573969738748273bef6e9ff7b
Thomas Weinandy <tomweinandy@gmail.com>
2022-08-26 10:13:29 -0400

    Update README, clean up notes, remove unused file

--- Projects/Day089/fun.py
+++
@@ -1,8 +0,0 @@
-"""
-Helper functions
-"""
-import datetime
-
-timestamp = datetime.datetime.now().strftime('%Y.%m.%d-%H.%M')
-text_file = open(f"../../../Downloads/writing_session_{timestamp}.txt", "w")
-text_file.write('Jello World')
