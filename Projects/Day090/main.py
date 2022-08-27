fd5869ea39410ded581a7ad1d50d859de3b955b7
Thomas Weinandy <tomweinandy@gmail.com>
2022-08-27 07:21:34 -0400

    Begin Day 90

---
+++ Projects/Day090/main.py
@@ -0,0 +1,9 @@
+"""
+Day 90: PDF to Audiobook
+
+Instructions: Using what you've learnt about HTTP requests, APIs and Python scripting, create a program that can convert PDF files to speech.
+You right want to choose your own Text-To-Speech (TTS) API. But here are some you can consider:
+http://www.ispeech.org/api/#introduction
+https://cloud.google.com/text-to-speech/docs/basics
+https://aws.amazon.com/polly/
+"""
