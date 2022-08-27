e99a7e9a49c4d678e1ed740ca0fbc3e4f5cd57e5
Thomas Weinandy <tomweinandy@gmail.com>
2022-08-27 15:09:33 -0400

    Get 'hello world' text to speech working

--- Projects/Day090/main.py
+++ Projects/Day090/main.py
@@ -7,3 +7,69 @@ http://www.ispeech.org/api/#introduction
 https://cloud.google.com/text-to-speech/docs/basics
 https://aws.amazon.com/polly/
 """
+# Starter code from https://docs.aws.amazon.com/polly/latest/dg/get-started-what-next.html
+from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
+from contextlib import closing
+import os
+import sys
+import subprocess
+from tempfile import gettempdir
+import json
+
+# Set secret key
+with open('../../../../Dropbox/100DaysOfCodePRIVATE/Day90Creds.json') as file:
+    creds = json.loads(file.read())
+
+# Define URLs for The Movie Data Base
+ACCESS_KEY = creds['access_key']
+SECRET_KEY = creds['secret_key']
+# SESSION_TOKEN = creds['session_token']
+REGION = 'us-east-1'
+
+# Create a client using the credentials and region defined in the [adminuser]
+# section of the AWS credentials file (~/.aws/credentials).
+# session = Session(profile_name="adminuser")
+session = Session(aws_access_key_id=ACCESS_KEY,
+                  aws_secret_access_key=SECRET_KEY,
+                  region_name=REGION)
+polly = session.client("polly")
+
+try:
+    # Request speech synthesis
+    response = polly.synthesize_speech(Text="Hello world!", OutputFormat="mp3", VoiceId="Joanna")
+except (BotoCoreError, ClientError) as error:
+    # The service returned an error, exit gracefully
+    print(error)
+    sys.exit(-1)
+
+# Access the audio stream from the response
+if "AudioStream" in response:
+    # Note: Closing the stream is important because the service throttles on the
+    # number of parallel connections. Here we are using contextlib.closing to
+    # ensure the close method of the stream object will be called automatically
+    # at the end of the with statement's scope.
+    with closing(response["AudioStream"]) as stream:
+        output = os.path.join(gettempdir(), "speech.mp3")
+
+        try:
+            # Open a file for writing the output as a binary stream
+            with open(output, "wb") as file:
+                file.write(stream.read())
+        except IOError as error:
+            # Could not write to file, exit gracefully
+            print(error)
+            sys.exit(-1)
+
+else:
+    # The response didn't contain audio data, exit gracefully
+    print("Could not stream audio")
+    sys.exit(-1)
+
+# Play the audio using the platform's default player
+if sys.platform == "win32":
+    os.startfile(output)
+else:
+    # The following works on macOS and Linux. (Darwin = mac, xdg-open = linux).
+    opener = "open" if sys.platform == "darwin" else "xdg-open"
+    subprocess.call([opener, output])
