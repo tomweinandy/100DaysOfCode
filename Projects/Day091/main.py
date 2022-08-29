56e76f4f4c692d1d7bab0f2925ee078549591336
Thomas Weinandy <tomweinandy@gmail.com>
2022-08-29 08:20:33 -0400

    Add demo image to landing page

--- Projects/Day091/main.py
+++ Projects/Day091/main.py
@@ -1,5 +1,12 @@
 """
-Migrate main2.py over to here
+Day 91: Website to
+
+One of my favourite websites to go to when I'm designing anything is a colour palette website called Flat UI Colors.
+https://flatuicolors.com/palette/defo
+It's a really simple static website that shows a bunch of colours and their HEX codes. I can copy the HEX codes and use it in my CSS or any design software.
+On day 76, you learnt about image processing with NumPy. Using this knowledge and your developer skills (that means Googling), build a website where a user can upload an image and you will tell them what are the top 10 most common colours in that image.
+This is a good example of this functionality:
+http://www.coolphptools.com/color_extract#demo
 """
 from flask import Flask, render_template, url_for, redirect
 from flask_bootstrap import Bootstrap
@@ -9,6 +16,44 @@ from wtforms.validators import DataRequired, URL
 import csv
 import pandas as pd
 import numpy as np
+import colorgram
+
+# todo make landing page for akira photo
+# todo add photo upload
+
+# Convert rbg colors to hex
+def rgb_to_hex(rgb):
+    return '%02x%02x%02x' % rgb
+
+
+# Extract colors from the jpg
+# rgb_list = []
+# proportion_list = []
+color_list = []
+colors = colorgram.extract('static/img/akira.jpg', 10)
+
+# Convert colors to a list of RGB values
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    hex = rgb_to_hex((r, g, b))
+    proportion = round(color.proportion, 3)
+    color_tuple = ((r, g, b), hex, proportion)
+    color_list.append(color_tuple)
+
+    # rgb = (r, g, b)
+    # rgb_list.append(rgb)
+
+    # Store values for proportion of each color
+    # prop = round(color.proportion, 3)
+    # proportion_list.append(prop)
+
+# Make another list of hex color values
+# hex_list = [rgb_to_hex(c) for c in rgb_list]
+
+# Bundle it all together
+# colorgram = (rgb_list, hex_list, proportion_list)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -42,7 +87,7 @@ def add_cafe_to_csv(cafe, location, open, close, coffee, wifi, power):
 # all Flask routes below
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", color_list=color_list)
 
 
 @app.route('/add', methods=['POST', 'GET'])
