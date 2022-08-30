b96369051f1f35cfe2c22b1673dd0b7d1a72ac77
Thomas Weinandy <tomweinandy@gmail.com>
2022-08-30 04:59:31 -0400

    Upload submitted image

--- Projects/Day091/main.py
+++ Projects/Day091/main.py
@@ -8,17 +8,18 @@ On day 76, you learnt about image processing with NumPy. Using this knowledge an
 This is a good example of this functionality:
 http://www.coolphptools.com/color_extract#demo
 """
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
+# from werkzeug import secure_filename
 import csv
 import pandas as pd
 import numpy as np
 import colorgram
 
-# todo make landing page for akira photo
+
 # todo add photo upload
 
 # Convert rbg colors to hex
@@ -27,77 +28,75 @@ def rgb_to_hex(rgb):
 
 
 # Extract colors from the jpg
-# rgb_list = []
-# proportion_list = []
-color_list = []
-colors = colorgram.extract('static/img/akira.jpg', 10)
+def extract_colors(img, num):
+    color_list = []
+    colors = colorgram.extract(img, num)
 
-# Convert colors to a list of RGB values
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    hex = rgb_to_hex((r, g, b))
-    proportion = round(color.proportion, 3)
-    color_tuple = ((r, g, b), hex, proportion)
-    color_list.append(color_tuple)
+    # Convert colors to a list of RGB values
+    for color in colors:
+        r = color.rgb.r
+        g = color.rgb.g
+        b = color.rgb.b
+        hex = rgb_to_hex((r, g, b))
+        proportion = round(color.proportion, 3)
+        color_tuple = ((r, g, b), hex, proportion)
+        color_list.append(color_tuple)
 
-    # rgb = (r, g, b)
-    # rgb_list.append(rgb)
+    return color_list
 
-    # Store values for proportion of each color
-    # prop = round(color.proportion, 3)
-    # proportion_list.append(prop)
 
-# Make another list of hex color values
-# hex_list = [rgb_to_hex(c) for c in rgb_list]
-
-# Bundle it all together
-# colorgram = (rgb_list, hex_list, proportion_list)
+akira_colors = extract_colors('static/img/akira.jpg', 13)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
 
-class CafeForm(FlaskForm):
+class PhotoForm(FlaskForm):
     cafe = StringField('Café name', validators=[DataRequired()])
     location = StringField('Location', validators=[DataRequired()])
     open = StringField('Open', validators=[DataRequired()])
     close = StringField('Close', validators=[DataRequired()])
 
-    coffee_choices = ['☕️', '☕️☕️', '☕️☕️☕️', '☕️☕️☕️☕️', '☕️☕️☕️☕️☕️', '✘']
-    wifi_choices = ['💪', '💪💪', '💪💪💪', '💪💪💪💪', '💪💪💪💪💪', '✘']
-    power_choices = ['🔌', '🔌🔌', '🔌🔌🔌', '🔌🔌🔌🔌', '🔌🔌🔌🔌🔌', '✘']
-
-    coffee = SelectField('Coffee', choices=coffee_choices, validators=[DataRequired()])
-    wifi = SelectField('Wifi', choices=wifi_choices, validators=[DataRequired()])
-    power = SelectField('Power', choices=power_choices, validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    # coffee = SelectField('Coffee', choices=coffee_choices, validators=[DataRequired()])
+    # wifi = SelectField('Wifi', choices=wifi_choices, validators=[DataRequired()])
+    # power = SelectField('Power', choices=power_choices, validators=[DataRequired()])
+    submit = SubmitField('Run')
 
 
-def add_cafe_to_csv(cafe, location, open, close, coffee, wifi, power):
-    df = pd.read_csv('cafe-data.csv')
-    new_cafe = np.array([[cafe, location, open, close, coffee, wifi, power]])
-    new_entry = pd.DataFrame(data=new_cafe, columns=df.columns)
-    df = pd.concat([df, new_entry])
-    df.to_csv('cafe-data.csv', index=False)
+# def add_cafe_to_csv(cafe, location, open, close, coffee, wifi, power):
+#     df = pd.read_csv('cafe-data.csv')
+#     new_cafe = np.array([[cafe, location, open, close, coffee, wifi, power]])
+#     new_entry = pd.DataFrame(data=new_cafe, columns=df.columns)
+#     df = pd.concat([df, new_entry])
+#     df.to_csv('cafe-data.csv', index=False)
 
 
 # all Flask routes below
 @app.route("/")
 def home():
-    return render_template("index.html", color_list=color_list)
+    return render_template("index.html", color_list=akira_colors)
 
 
-@app.route('/add', methods=['POST', 'GET'])
-def add_cafe():
-    form = CafeForm()
-    if form.validate_on_submit():
-        add_cafe_to_csv(form.cafe.data, form.location.data, form.open.data,
-                        form.close.data, form.coffee.data, form.wifi.data, form.power.data)
-        return redirect(url_for('add_cafe'))
-    return render_template('add.html', form=form)
+# @app.route('/add', methods=['POST', 'GET'])
+# def add_cafe():
+#     form = CafeForm()
+#     if form.validate_on_submit():
+#         add_cafe_to_csv(form.cafe.data, form.location.data, form.open.data,
+#                         form.close.data, form.coffee.data, form.wifi.data, form.power.data)
+#         return redirect(url_for('add_cafe'))
+#     return render_template('add.html', form=form)
+
+
+@app.route('/color_extract', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['imgFile']
+        f.save(f.filename)
+        c = extract_colors(f, 10)
+        print(c)
+        # f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
 
 
 @app.route('/cafes')
