45abfc130a5d86f59d613401170230e6b821add2
Thomas Weinandy <tomweinandy@gmail.com>
2022-09-01 11:22:03 -0400

    Added jumping logic

--- Projects/Day093/main.py
+++ Projects/Day093/main.py
@@ -13,11 +13,15 @@ import pyautogui
 import webbrowser
 import time
 import keyboard
+import colorgram
+
+SCORE_BOX = 'score_box.png'
+SCAN_BOX = 'scan_box.png'
 
 
 def locate():
-    currentMouseX, currentMouseY = pyautogui.position()
-    print('Current mouse location:', currentMouseX, currentMouseY)
+    current_mouse_x, current_mouse_y = pyautogui.position()
+    print('Current mouse location:', current_mouse_x, current_mouse_y)
 
 
 # Get the size of the primary monitor
@@ -38,29 +42,45 @@ time.sleep(5)
 pyautogui.moveTo(100, 150)
 locate()
 
+# NOTE: Had to change system preferences to allow my IDE to control the keyboard and take screenshots # todo move note
+
 # Find the position of the game title (used to locate other objects)
 title_location = pyautogui.locateOnScreen('title.png')
 left, top, width, height = title_location
 
-# NOTE: Had to change system preferences to allow my IDE to control the keyboard
-# Jump every second
-for i in range(5):
-    keyboard.send('space')
-    time.sleep(1)
+# Define scan and score boxes (subtract from left to move left, add to top to move down)
+scan_box = (left+150, top+500, width-400, height+50)
+score_box = (left+475, top+280, width-150, height+50)
 
-# Take screenshot of browser
-pyautogui.screenshot('screenshot.png')
+last_black_ink = 0
 
-# Take screenshot of area in front of dino
-# subtract from left to move left, add to top to move down
-scan_box = (left-150, top+500, width-500, height+50)
-pyautogui.screenshot('scan_box.png', region=scan_box)
+# Start game
+keyboard.send('space')
+for i in range(20):
+    time.sleep(0.1) # todo try removing time
+
+    # Take screenshot of area in front of dino
+    pyautogui.screenshot(SCAN_BOX, region=scan_box)
+
+    # Find share pixels within scan box that are black (indicates cactus)
+    # Extract colors from scan box
+    colors = colorgram.extract(SCAN_BOX, 2)
+    # Take last (ie, second) color which will always be black
+    black_ink = colors[-1].proportion
+    print(black_ink)
+
+    # Make dino jump if share of black pixels has changed (indicates screen movement) and meets a threshold
+    if black_ink != last_black_ink and black_ink > 0.03:
+        keyboard.send('space')
+        print('JUMP')
+
+    last_black_ink = black_ink
+
 
 # Take screenshot of score
-score_box = (left+475, top+280, width-150, height+50)
-pyautogui.screenshot('score_box.png', region=score_box)
+pyautogui.screenshot(SCORE_BOX, region=score_box)
 
-# todo Locate pixels within image
+
 
 # todo Record score
 
