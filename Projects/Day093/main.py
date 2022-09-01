7c4b0c20f8736606f89a6a886da81e627f227562
Thomas Weinandy <tomweinandy@gmail.com>
2022-09-01 06:05:54 -0400

    Began Day 93

---
+++ Projects/Day093/main.py
@@ -0,0 +1,11 @@
+"""
+Day 93: T-Rex Run Game Player
+
+Alternatively you can access the game directly here:
+https://elgoog.im/t-rex/
+Your goal today is to write a Python script to automate the playing of this game. Your program will look at the pixels on the screen to determine when it needs to hit the space bar and play the game automatically.
+
+You might want to look up these two packages:
+https://pypi.org/project/Pillow/
+https://pyautogui.readthedocs.io/en/latest/
+"""
