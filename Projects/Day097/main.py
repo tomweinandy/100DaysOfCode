67a5a06cab808d78421fff5f4ee37f74a30e3c60
Thomas Weinandy <tomweinandy@gmail.com>
2022-09-19 07:22:39 -0400

    Add salutation function and correct edge cases

--- Projects/Day097/main.py
+++ Projects/Day097/main.py
@@ -8,21 +8,87 @@ import datetime as dt
 import random
 import pandas as pd
 import json
+import textract
+
+
+def salutation(contact_series):
+    """
+    Converts a contact series into a proper salutation depending on the number of people.
+    :param contact_series: Series taken from the subset of all contacts from the same organziation
+    :return: A string salutation
+    """
+    # Convert to list
+    contact_list = []
+    for contact in contact_series:
+        # Fill non-string names with "Colleague"
+        if type(contact) != str:
+            contact = 'Colleague'
+
+        # Remove leading/trailing whitespace
+        contact = contact.strip()
+
+        # Fill in empty string names with "Colleague"
+        if len(contact) == 0:
+            contact = 'Colleague'
+        contact_list.append(contact)
+
+    # If there is only one contact
+    if len(contact_list) == 1:
+        contact_str = contact_list[0]
+
+    # If two contacts
+    elif len(contact_series) == 2:
+        contact_str = contact_list[0] + ' and ' + contact_list[1]
+
+    # If more than two contacts
+    else:
+        # Put first contact at the end
+        contact_str = 'and ' + contact_list[0]
+        # Add remaining contacts
+        for contact in contact_list[1:]:
+            contact_str = contact.strip() + ', ' + contact_str
+
+    return contact_str
+
+
+# Define folder path of project assets
+folder_path = '../../../../Dropbox/Big Data Ignite/AutomatedEmail/'
 
 # Read in credentials and save as a dictionary
-with open('../../../../Dropbox/100DaysOfCodePRIVATE/Day97Creds.json') as file:
+with open(folder_path + 'Creds.json') as file:
     creds = json.loads(file.read())
 
 EMAIL = creds['EMAIL']
 EMAIL_KEY = creds['EMAIL_KEY']
 
 # Read in list of potential sponsors
-# df = pd.read_csv('../../../../Dropbox/Big Data Ignite/PotentialSponsors.csv')
+# df = pd.read_csv(folder_path + 'PotentialSponsors.csv')
 df = pd.read_csv('TestSponsors.csv')
 
+# Define list of all organizations in the csv
+org_list = df['Organization'].unique()
 
-for idx, row in df.iterrows():
-    print(row['Contact Name'])
+# Read in message for email
+text_byte = textract.process(folder_path + 'Sponsorship Invitation - Cold Email.docx')
+text = text_byte.decode("utf-8")
+
+
+for org in org_list:
+    df_subset = df[df['Organization'] == org]
+    df_subset = df_subset.reset_index(drop=True)
+
+    print(df_subset['Organization'][0])
+    print(salutation(df_subset['Contact Name']))
+
+
+
+
+    #     print()
+    #
+    # new_text = text.replace('<Contact Name>', contacts)
+    # new_text = new_text.replace('<Organization Name>', org)
+    # print(new_text)
+
 
 
 
