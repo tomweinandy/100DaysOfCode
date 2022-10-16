0e7714acf60fe6f487a992ae948eba70ecc3d974
Thomas Weinandy <tomweinandy@gmail.com>
2022-10-16 13:54:24 -0400

    Clean up code, add/remove photos

--- Projects/Day082/main.py
+++ Projects/Day082/main.py
@@ -14,8 +14,6 @@ from flask_gravatar import Gravatar
 from functools import wraps
 import csv
 
-# todo update project section
-# todo clean up code
 # todo get contact form to work
 # todo update final/project photo?
 # todo remove unused files (esp. photos)
@@ -27,7 +25,7 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-# CONNECT TO CSV
+# Connect to the CSV with project details
 with open('projects.csv', newline='') as csv_file:
     csv_data = csv.reader(csv_file, delimiter=',')
     list_of_dicts = []
@@ -43,172 +41,14 @@ with open('projects.csv', newline='') as csv_file:
     # print(type(csv_file), type(csv_data), type(list_of_rows), type(row))
     # print(list_of_dicts)
 
-# # CONNECT TO DB
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///this_old_thing.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
 
-
-# # CONFIGURE TABLES
-# class ProductPost(db.Model):
-#     __tablename__ = "products"
-#     id = db.Column(db.Integer, primary_key=True)
-#
-#     # Create Foreign Key, "users.id" the users refers to the tablename of User.
-#     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-#     # Create reference to the User object, the "posts" refers to the post's property in the User class.
-#     author = relationship("User", back_populates="posts")
-#
-#     title = db.Column(db.String(250), unique=True, nullable=False)
-#     price = db.Column(db.String(250), nullable=False)
-#     date = db.Column(db.String(250), nullable=False)
-#     body = db.Column(db.Text, nullable=False)
-#     img_url = db.Column(db.String(250), nullable=False)
-#     comments = relationship("Comment", back_populates="parent_post")
-#
-#
-# # Create the User Table
-# class User(UserMixin, db.Model):
-#     __tablename__ = "users"
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(100), unique=True)
-#     password = db.Column(db.String(100))
-#     name = db.Column(db.String(100))
-#
-#     # This will act like a List of ProductPost objects attached to each User.
-#     # The "author" refers to the author property in the ProductPost class.
-#     posts = relationship("ProductPost", back_populates="author")
-#
-#     # "comment_author" refers to the comment_author property in the Comment class.
-#     comments = relationship("Comment", back_populates="comment_author")
-#
-#
-# # Create a Comments Table
-# class Comment(db.Model):
-#     __tablename__ = "comments"
-#     id = db.Column(db.Integer, primary_key=True)
-#     text = db.Column(db.Text, nullable=False)
-#
-#     # "users.id" The users refers to the tablename of the Users class.
-#     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-#
-#     # "comments" refers to the comment's property in the User class.
-#     comment_author = relationship("User", back_populates="comments")
-#
-#     # child relationship
-#     post_id = db.Column(db.Integer, db.ForeignKey("products.id"))
-#     parent_post = relationship("ProductPost", back_populates="comments")
-#     text = db.Column(db.Text, nullable=False)
-#
-#
-# # Create a form for commenting on product posts
-# class CommentForm(FlaskForm):
-#     comment_text = CKEditorField("Comment", validators=[DataRequired()])
-#     submit = SubmitField("Submit Comment")
-#
-#
-# # Add Gravatar images
-# gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
-#                     base_url=None)
-#
-# # Create all the tables in the database
-# db.create_all()
-#
-#
-# # Create admin-only decorator
-# def admin_only(f):
-#     @wraps(f)
-#     def decorated_function(*args, **kwargs):
-#         # If id is not 1 then return abort with 403 error
-#         if current_user.id != 1:
-#             return abort(403)
-#         # Otherwise continue with the route function
-#         return f(*args, **kwargs)
-#
-#     return decorated_function
-
-
+# Display landing page
 @app.route('/', methods=['GET', 'POST'])
 def get_all_posts():
-    # posts = ProductPost.query.all()
-    # return render_template("index.html", all_posts=posts, current_user=current_user)
     return render_template("index.html")
 
 
-# # Register new users into the User database
-# @app.route('/register', methods=["GET", "POST"])
-# def register():
-#     form = RegisterForm()
-#
-#     if form.validate_on_submit():
-#         # Check if user already exists
-#         if User.query.filter_by(email=request.form.get('email')).first():
-#             flash('You have already signed up with that email. Please log in instead.')
-#             return redirect(url_for('login'))
-#
-#         hash_and_salted_password = generate_password_hash(
-#             form.password.data,
-#             method='pbkdf2:sha256',
-#             salt_length=8
-#         )
-#         new_user = User(
-#             email=form.email.data,
-#             name=form.name.data,
-#             password=hash_and_salted_password,
-#         )
-#         db.session.add(new_user)
-#         db.session.commit()
-#
-#         # This line will authenticate the user with Flask-Login
-#         login_user(new_user)
-#
-#         return redirect(url_for("get_all_posts"))
-#
-#     return render_template("register.html", form=form, current_user=current_user)
-
-
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-
-
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
-
-
-# @app.route('/login', methods=["GET", "POST"])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         email = form.email.data
-#         password = form.password.data
-#
-#         # Find user by email
-#         user = User.query.filter_by(email=email).first()
-#
-#         # Check if email exists
-#         if not user:
-#             flash('That email does not exist. Please try again.')
-#             return redirect(url_for('login'))
-#
-#         # Check if password correct
-#         elif user and not check_password_hash(user.password, password):
-#             flash('Password incorrect, please try again.')
-#             return redirect(url_for('login'))
-#
-#         else:
-#             login_user(user)
-#             return redirect(url_for('get_all_posts'))
-#
-#     return render_template("login.html", form=form, current_user=current_user)
-
-
-# @app.route('/logout')
-# def logout():
-#     logout_user()
-#     return redirect(url_for('get_all_posts'))
-
-
+# Display project page
 @app.route("/day/<day_number>", methods=["GET", "POST"])
 def show_day(day_number):
     print("day_number", type(day_number), day_number)
@@ -217,109 +57,6 @@ def show_day(day_number):
             selected_project = project
     print('Day:', selected_project)
     return render_template("portfolio-details.html", project=selected_project)
-
-
-# def show_post(post_id):
-#     form = CommentForm()
-#     requested_post = ProductPost.query.get(post_id)
-#
-#     if form.validate_on_submit():
-#         if not current_user.is_authenticated:
-#             flash("You need to login or register to comment.")
-#             return redirect(url_for("login"))
-#
-#         new_comment = Comment(
-#             text=form.comment_text.data,
-#             comment_author=current_user,
-#             parent_post=requested_post
-#         )
-#         db.session.add(new_comment)
-#         db.session.commit()
-#
-#     return render_template("post.html", post=requested_post, form=form, current_user=current_user)
-
-
-# @app.route("/about")
-# def about():
-#     return render_template("about.html", current_user=current_user)
-
-
-# @app.route("/confirmation")
-# def confirmation():
-#     return render_template("confirmation.html")
-
-
-# @app.route("/contact")
-# def contact():
-#     return render_template("contact.html", current_user=current_user)
-
-
-# @app.route("/new-post", methods=['GET', 'POST'])
-# @admin_only
-# def add_new_post():
-#     print('Checkpoint 1')
-#     form = CreatePostForm()
-#     print('Checkpoint 2')
-#     print(form)
-#     print(form.title.data,
-#           form.price.data,
-#           form.img_url.data,
-#           form.body.data,
-#           date.today().strftime("%B %d, %Y"),
-#           type(current_user),
-#           form.validate_on_submit()
-#           )
-#
-#     if form.validate_on_submit():
-#         print('Checkpoint 3')
-#         new_post = ProductPost(
-#             title=form.title.data,
-#             price=form.price.data,
-#             body=form.body.data,
-#             img_url=form.img_url.data,
-#             author=current_user,
-#             date=date.today().strftime("%B %d, %Y")
-#         )
-#         print('Checkpoint 4')
-#         db.session.add(new_post)
-#         print('Checkpoint 5')
-#         db.session.commit()
-#         print('Checkpoint 6')
-#         return redirect(url_for("get_all_posts"))
-#     print('Checkpoint 7')
-#     return render_template("make-post.html", form=form, current_user=current_user)
-
-
-# @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
-# @admin_only
-# def edit_post(post_id):
-#     post = ProductPost.query.get(post_id)
-#     edit_form = CreatePostForm(
-#         title=post.title,
-#         price=post.price,
-#         img_url=post.img_url,
-#         author=post.author,
-#         body=post.body
-#     )
-#     if edit_form.validate_on_submit():
-#         post.title = edit_form.title.data
-#         post.price = edit_form.price.data
-#         post.img_url = edit_form.img_url.data
-#         # post.author = edit_form.author.data
-#         post.body = edit_form.body.data
-#         db.session.commit()
-#         return redirect(url_for("show_post", post_id=post.id))
-#
-#     return render_template("make-post.html", form=edit_form, is_edit=True, current_user=current_user)
-
-
-# @app.route("/delete/<int:post_id>")
-# @admin_only
-# def delete_post(post_id):
-#     post_to_delete = ProductPost.query.get(post_id)
-#     db.session.delete(post_to_delete)
-#     db.session.commit()
-#     return redirect(url_for('get_all_posts'))
 
 
 if __name__ == "__main__":
