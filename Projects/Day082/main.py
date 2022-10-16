9d5441e441309990ee0033a13fb28d787921a394
Thomas Weinandy <tomweinandy@gmail.com>
2022-10-16 14:04:09 -0400

    Final edits before testing day 82 deployment

--- Projects/Day082/main.py
+++ Projects/Day082/main.py
@@ -1,24 +1,10 @@
 """
 Day 82: Portfolio Website
 """
-from flask import Flask, render_template, redirect, url_for, flash, request, abort
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
-from datetime import date
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-# from forms import CreatePostForm, RegisterForm, LoginForm, FlaskForm, CKEditorField, SubmitField, DataRequired
-from flask_gravatar import Gravatar
-from functools import wraps
 import csv
-
-# todo get contact form to work
-# todo update final/project photo?
-# todo remove unused files (esp. photos)
-# todo update stats
-# todo deploy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
